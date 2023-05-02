3c9c753d7458a7924e08b035445e5a866d3502b7
Makoto Morita <makmorit@outlook.jp>
2023-05-02 19:06:57 +0900

    デスクトップツールの初版制作
    ・アプリケーション本体を実装

---
+++ DesktopTools/dotNET/DesktopToolApp/DesktopToolApp/App.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading;
+using System.Windows;
+using static DesktopTool.AppMessages;
+
+namespace DesktopToolApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        // Mutex作成
+        private readonly Mutex MutexRef = new Mutex(false, "DesktopToolApp");
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            // Mutexの所有権を要求
+            if (MutexRef.WaitOne(0, false) == false) {
+                MessageBox.Show(MSG_ERROR_DOUBLE_START, MSG_TOOL_TITLE);
+                MutexRef.Close();
+                Shutdown();
+            }
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            // TODO: 業務終了時の処理
+
+            // Mutexを解放
+            if (MutexRef != null) {
+                MutexRef.ReleaseMutex();
+                MutexRef.Close();
+            }
+            base.OnExit(e);
+        }
+    }
+}
---
+++ DesktopTools/dotNET/DesktopToolApp/DesktopToolApp/AppMessages.cs
@@ -0,0 +1,8 @@
+﻿namespace DesktopTool
+{
+    public static class AppMessages
+    {
+        public const string MSG_TOOL_TITLE = "デスクトップツール";
+        public const string MSG_ERROR_DOUBLE_START = "既に起動されています。";
+    }
+}
