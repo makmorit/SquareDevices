5c028964735d1f76a7526d63b051dc5d7f7e6f06
Makoto Morita <makmorit@outlook.jp>
2023-05-03 16:10:33 +0900

    ベンダーツールの初版制作
    ・アプリケーション本体を実装

--- DesktopTools/dotNET/DesktopToolApp/DesktopToolApp/AppMessages.cs
+++ DesktopTools/dotNET/DesktopToolApp/DesktopToolApp/AppMessages.cs
@@ -5,5 +5,8 @@
         public const string MSG_TOOL_TITLE = "デスクトップツール";
         public const string MSG_VENDOR_TOOL_TITLE = "ベンダーツール";
         public const string MSG_ERROR_DOUBLE_START = "既に起動されています。";
+
+        // ベンダー向け機能関連
+        public const string MSG_MENU_ITEM_NAME_VENDOR_FUNCTION = "ベンダー向け機能";
     }
 }
