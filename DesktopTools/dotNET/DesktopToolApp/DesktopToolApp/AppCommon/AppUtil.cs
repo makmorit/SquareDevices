1561c2cf7e2b74cf9e3d94a2cb92140a97387f56
Makoto Morita <makmorit@outlook.jp>
2023-05-03 15:37:37 +0900

    リファクタリング
    ・不要な処理を削除

--- DesktopTools/dotNET/DesktopToolApp/DesktopToolApp/AppCommon/AppUtil.cs
+++ DesktopTools/dotNET/DesktopToolApp/DesktopToolApp/AppCommon/AppUtil.cs
@@ -1,82 +1,12 @@
 ﻿using DesktopTool;
 using System;
 using System.Linq;
-using System.Reflection;
 using System.Text.RegularExpressions;
-using static DesktopTool.AppMessages;
 
 namespace AppCommon
 {
     public class AppUtil
     {
-        private static readonly AppUtil Instance = new AppUtil();
-        private readonly string AppVersion;
-        private readonly string AppCopyright;
-        private readonly string AppBundleName;
-        private readonly string AppTitle;
-
-        private AppUtil()
-        {
-            // ツールのバージョン、著作権情報を取得
-            AppVersion = string.Format("Version {0}", GetAppVersion());
-            AppCopyright = GetAppCopyright();
-            AppBundleName = GetAppBundleName();
-
-            // メイン画面のタイトルを設定
-            if (IsVendorDesktopTool()) {
-                AppTitle = MSG_VENDOR_TOOL_TITLE;
-            } else {
-                AppTitle = MSG_TOOL_TITLE;
-            }
-        }
-
-        private static string GetAppVersion()
-        {
-            // 製品バージョン文字列を戻す
-            Assembly asm = Assembly.GetExecutingAssembly();
-            System.Diagnostics.FileVersionInfo ver = System.Diagnostics.FileVersionInfo.GetVersionInfo(asm.Location);
-            string? versionString = ver.ProductVersion;
-            if (versionString == null) {
-                return string.Empty;
-            } else {
-                return versionString;
-            }
-        }
-
-        private static string GetAppCopyright()
-        {
-            // 著作権情報を戻す
-            Assembly asm = Assembly.GetExecutingAssembly();
-            Attribute? attribute = Attribute.GetCustomAttribute(asm, typeof(AssemblyCopyrightAttribute));
-            if (attribute == null) {
-                return string.Empty;
-            }
-            AssemblyCopyrightAttribute copyright = (AssemblyCopyrightAttribute)attribute;
-            return copyright.Copyright;
-        }
-
-        private static string GetAppBundleName()
-        {
-            // アプリケーションバンドル文字列を戻す
-            AssemblyName assemblyName = Assembly.GetExecutingAssembly().GetName();
-            if (assemblyName.Name == null) {
-                return string.Empty;
-            } else {
-                return assemblyName.Name;
-            }
-        }
-
-        public static string GetApplicationName()
-        {
-            // 製品名の文字列を戻す
-            AssemblyProductAttribute attribute = Assembly.GetExecutingAssembly().GetCustomAttribute<AssemblyProductAttribute>()!;
-            if (attribute == null) {
-                return "DesktopToolApp";
-            } else {
-                return attribute.Product;
-            }
-        }
-
         //
         // ログ関連
         //
@@ -92,34 +22,6 @@ namespace AppCommon
         {
             // アプリケーション終了ログを出力
             AppLogUtil.OutputLogInfo(string.Format("{0}を終了しました", AppInfoUtil.GetAppTitleString()));
-        }
-
-        public static bool IsVendorDesktopTool()
-        {
-            return GetApplicationName().Equals("VendorDesktopTool");
-        }
-
-        //
-        // 公開用メソッド
-        //
-        public static string GetAppVersionString()
-        {
-            return Instance.AppVersion;
-        }
-
-        public static string GetAppCopyrightString()
-        {
-            return Instance.AppCopyright;
-        }
-
-        public static string GetAppBundleNameString()
-        {
-            return Instance.AppBundleName;
-        }
-
-        public static string GetAppTitleString()
-        {
-            return Instance.AppTitle;
         }
 
         // 
