200e8d9dff5de040fceb70dc1f172dcba6cf75f0
Makoto Morita <makmorit@outlook.jp>
2023-05-03 11:31:16 +0900

    デスクトップツールの初版制作
    ・ユーティリティークラスの追加

---
+++ DesktopTools/dotNET/DesktopToolApp/DesktopToolApp/AppCommon/AppLogUtil.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace AppCommon
+{
+    public class AppLogUtil
+    {
+        private static AppLogUtil Instance = new AppLogUtil();
+        private string ApplicationName = "";
+
+        private AppLogUtil()
+        {
+        }
+
+        //
+        // 公開用メソッド
+        //
+        public static void SetApplicationName(string applicationName)
+        {
+            Instance.ApplicationName = applicationName;
+        }
+
+        public static string GetApplicationName()
+        {
+            return Instance.ApplicationName;
+        }
+
+        public static void OutputLogText(string logText, string fname)
+        {
+            try {
+                // ログファイルにメッセージを出力する
+                StreamWriter sr = new StreamWriter(new FileStream(fname, FileMode.Append), Encoding.Default);
+                sr.WriteLine(logText);
+                sr.Close();
+
+            } catch (Exception e) {
+                Console.Write(e.Message);
+            }
+        }
+
+        public static void SetOutputLogApplName(string applName)
+        {
+            SetApplicationName(applName);
+        }
+
+        public static void OutputLogText(string logText)
+        {
+            // アプリケーション名が未設定の場合はログを出力させない
+            if (GetApplicationName().Length == 0) {
+                return;
+            }
+
+            // ログファイルにメッセージを出力する
+            string fname = string.Format("{0}\\{1}.log", OutputLogFileDirectoryPath(), GetApplicationName());
+            OutputLogText(logText, fname);
+        }
+
+        public static string OutputLogFileDirectoryPath()
+        {
+            try {
+                // ホームディレクトリー配下に生成
+                string dir = string.Format("{0}\\makmorit\\SquareDevices",
+                    Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData));
+
+                // ディレクトリー存在チェック
+                if (Directory.Exists(dir) == false) {
+                    // ディレクトリーが存在しない場合は新規生成
+                    DirectoryInfo dirInfo = Directory.CreateDirectory(dir);
+                    Console.Write(string.Format("outputLogText: Directory created at {0}", dir));
+                }
+
+                // ディレクトリーを戻す
+                return dir;
+
+            } catch (Exception e) {
+                Console.Write(e.Message);
+                return ".";
+            }
+        }
+
+        public static void OutputLogError(string message)
+        {
+            // メッセージに現在時刻を付加し、ログファイルに出力
+            OutputLogText(string.Format("{0} [error] {1}", DateTime.Now.ToString(), message));
+        }
+
+        public static void OutputLogWarn(string message)
+        {
+            // メッセージに現在時刻を付加し、ログファイルに出力
+            OutputLogText(string.Format("{0} [warn] {1}", DateTime.Now.ToString(), message));
+        }
+
+        public static void OutputLogInfo(string message)
+        {
+            // メッセージに現在時刻を付加し、ログファイルに出力
+            OutputLogText(string.Format("{0} [info] {1}", DateTime.Now.ToString(), message));
+        }
+
+        public static void OutputLogDebug(string message)
+        {
+            // メッセージに現在時刻を付加し、ログファイルに出力
+            OutputLogText(string.Format("{0} [debug] {1}", DateTime.Now.ToString(), message));
+        }
+
+        public static string DumpMessage(byte[] message, int length)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < length; i++) {
+                sb.Append(string.Format("{0:x2} ", message[i]));
+                if ((i % 16 == 15) && (i < length - 1)) {
+                    sb.Append("\r\n");
+                }
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ DesktopTools/dotNET/DesktopToolApp/DesktopToolApp/AppCommon/AppUtil.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Text.RegularExpressions;
+
+namespace AppCommon
+{
+    public class AppUtil
+    {
+        private static readonly AppUtil Instance = new AppUtil();
+        private readonly string AppVersion;
+        private readonly string AppCopyright;
+        private readonly string AppBundleName;
+
+        private AppUtil()
+        {
+            // ツールのバージョン、著作権情報を取得
+            AppVersion = string.Format("Version {0}", GetAppVersion());
+            AppCopyright = GetAppCopyright();
+            AppBundleName = GetAppBundleName();
+        }
+
+        private static string GetAppVersion()
+        {
+            // 製品バージョン文字列を戻す
+            Assembly asm = Assembly.GetExecutingAssembly();
+            System.Diagnostics.FileVersionInfo ver = System.Diagnostics.FileVersionInfo.GetVersionInfo(asm.Location);
+            string? versionString = ver.ProductVersion;
+            if (versionString == null) {
+                return string.Empty;
+            } else {
+                return versionString;
+            }
+        }
+
+        private static string GetAppCopyright()
+        {
+            // 著作権情報を戻す
+            Assembly asm = Assembly.GetExecutingAssembly();
+            Attribute? attribute = Attribute.GetCustomAttribute(asm, typeof(AssemblyCopyrightAttribute));
+            if (attribute == null) {
+                return string.Empty;
+            }
+            AssemblyCopyrightAttribute copyright = (AssemblyCopyrightAttribute)attribute;
+            return copyright.Copyright;
+        }
+
+        private static string GetAppBundleName()
+        {
+            // アプリケーションバンドル文字列を戻す
+            AssemblyName assemblyName = Assembly.GetExecutingAssembly().GetName();
+            if (assemblyName.Name == null) {
+                return string.Empty;
+            } else {
+                return assemblyName.Name;
+            }
+        }
+
+        //
+        // 公開用メソッド
+        //
+        public static string GetAppVersionString()
+        {
+            return Instance.AppVersion;
+        }
+
+        public static string GetAppCopyrightString()
+        {
+            return Instance.AppCopyright;
+        }
+
+        public static string GetAppBundleNameString()
+        {
+            return Instance.AppBundleName;
+        }
+
+        // 
+        // ユーティリティー
+        //
+        public static int CalculateDecimalVersion(string versionStr)
+        {
+            // バージョン文字列 "1.2.11" -> "010211" 形式に変換
+            int decimalVersion = 0;
+            foreach (string element in versionStr.Split('.')) {
+                decimalVersion = decimalVersion * 100 + int.Parse(element);
+            }
+            return decimalVersion;
+        }
+
+        public static bool CompareBytes(byte[] src, byte[] dest, int size)
+        {
+            for (int i = 0; i < size; i++) {
+                if (src[i] != dest[i]) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public static int ToInt32(byte[] value, int startIndex, bool changeEndian = false)
+        {
+            byte[] sub = GetSubArray(value, startIndex, 4);
+            if (changeEndian == true) {
+                sub = sub.Reverse().ToArray();
+            }
+            return BitConverter.ToInt32(sub, 0);
+        }
+
+        public static int ToInt16(byte[] value, int startIndex, bool changeEndian = false)
+        {
+            byte[] sub = GetSubArray(value, startIndex, 2);
+            if (changeEndian == true) {
+                sub = sub.Reverse().ToArray();
+            }
+            return BitConverter.ToInt16(sub, 0);
+        }
+
+        public static UInt32 ToUInt32(byte[] value, int startIndex, bool changeEndian = false)
+        {
+            byte[] sub = GetSubArray(value, startIndex, 4);
+            if (changeEndian == true) {
+                sub = sub.Reverse().ToArray();
+            }
+            return BitConverter.ToUInt32(sub, 0);
+        }
+
+        public static UInt16 ToUInt16(byte[] value, int startIndex, bool changeEndian = false)
+        {
+            byte[] sub = GetSubArray(value, startIndex, 2);
+            if (changeEndian == true) {
+                sub = sub.Reverse().ToArray();
+            }
+            return BitConverter.ToUInt16(sub, 0);
+        }
+
+        private static byte[] GetSubArray(byte[] src, int startIndex, int count)
+        {
+            byte[] dst = new byte[count];
+            Array.Copy(src, startIndex, dst, 0, count);
+            return dst;
+        }
+
+        public static void ConvertUint32ToLEBytes(UInt32 ui, byte[] b, int offset)
+        {
+            byte[] s = BitConverter.GetBytes(ui);
+            for (int i = 0; i < s.Length; i++) {
+                b[i + offset] = s[i];
+            }
+        }
+
+        public static void ConvertUint16ToLEBytes(UInt16 ui, byte[] b, int offset)
+        {
+            byte[] s = BitConverter.GetBytes(ui);
+            for (int i = 0; i < s.Length; i++) {
+                b[i + offset] = s[i];
+            }
+        }
+
+        public static void ConvertUint32ToBEBytes(UInt32 ui, byte[] b, int offset)
+        {
+            byte[] s = BitConverter.GetBytes(ui);
+            for (int i = 0; i < s.Length; i++) {
+                b[i + offset] = s[s.Length - 1 - i];
+            }
+        }
+
+        public static void ConvertUint16ToBEBytes(UInt16 ui, byte[] b, int offset)
+        {
+            byte[] s = BitConverter.GetBytes(ui);
+            for (int i = 0; i < s.Length; i++) {
+                b[i + offset] = s[s.Length - 1 - i];
+            }
+        }
+
+        public static string ReplaceCRLF(string src)
+        {
+            return new Regex("\r\n|\n").Replace(src, "");
+        }
+    }
+}
