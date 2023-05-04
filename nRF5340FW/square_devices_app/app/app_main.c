70473a59ae2a2241c443aa9661c4313b715749f9
Makoto Morita <makmorit@outlook.jp>
2023-05-04 18:06:02 +0900

    nRF5340ファームウェアの初版制作
    ・アプリケーション初期化処理

---
+++ nRF5340FW/square_devices_app/app/app_main.c
@@ -0,0 +1,46 @@
+/* 
+ * File:   app_main.c
+ * Author: makmorit
+ *
+ * Created on 2023/05/04, 18:05
+ */
+#include "app_ble_init.h"
+#include "app_board.h"
+#include "app_event.h"
+#include "app_main.h"
+#include "app_rtcc.h"
+#include "app_timer.h"
+#include "app_usb.h"
+
+//
+// アプリケーション初期化処理
+//
+void app_main_init(void) 
+{
+    // ボタン、LEDを使用可能にする
+    app_board_initialize();
+
+    // USBを使用可能にする
+    app_usb_initialize();
+
+    // タイマーを使用可能にする
+    app_timer_initialize();
+
+    // 業務処理イベント（APEVT_XXXX）を
+    // 通知できるようにする
+    app_event_main_enable(true);
+
+    // サブシステム初期化をメインスレッドで実行
+    app_event_notify(APEVT_SUBSYS_INIT);
+}
+
+void app_main_subsys_init(void)
+{
+    // リアルタイムクロックカレンダーの初期化
+    app_rtcc_initialize();
+
+    // Bluetoothサービス開始を指示
+    //   同時に、Flash ROMストレージが
+    //   使用可能となります。
+    app_ble_init();
+}
---
+++ nRF5340FW/square_devices_app/app/app_main.h
@@ -0,0 +1,24 @@
+/* 
+ * File:   app_main.h
+ * Author: makmorit
+ *
+ * Created on 2023/05/04, 18:05
+ */
+#ifndef APP_MAIN_H
+#define APP_MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+//
+// 関数群
+//
+void        app_main_init(void);
+void        app_main_subsys_init(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* APP_MAIN_H */
