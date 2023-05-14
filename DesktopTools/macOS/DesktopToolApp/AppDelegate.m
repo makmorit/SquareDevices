db1e96db52c9f7227fb378daea6ce422c03e4409
Makoto Morita <makmorit@outlook.jp>
2023-05-14 17:45:42 +0900

    Update AppDelegate.m
    ・カスタムメニューの項目を追加
    （仮の実装です。）

--- DesktopTools/macOS/DesktopToolApp/AppDelegate.m
+++ DesktopTools/macOS/DesktopToolApp/AppDelegate.m
@@ -9,10 +9,14 @@
 #import "ToolCommonFunc.h"
 #import "ToolLogFile.h"
 
-@interface AppDelegate ()
+@interface AppDelegate () <NSOutlineViewDelegate>
 
     @property (assign) IBOutlet NSWindow        *window;
     @property (assign) IBOutlet NSMenuItem      *menuItemVendor;
+
+    // カスタマイズしたサイドバーメニュー
+    @property(nonatomic, weak) IBOutlet NSOutlineView   *sidebar;
+    @property(nonatomic, strong) NSArray                *sidebarItems;
 
 @end
 
@@ -29,6 +33,29 @@
         }
         // アプリケーション開始ログを出力
         [[ToolLogFile defaultLogger] infoWithFormat:MSG_FORMAT_TOOL_LAUNCHED, [[self window] title], [ToolCommonFunc getAppVersionString], [ToolCommonFunc getAppBuildNumberString]];
+        // TODO: 仮の実装です。（カスタマイズしたサイドメニューを生成）
+        NSArray *array = [NSArray arrayWithObjects:
+                            [NSDictionary dictionaryWithObjectsAndKeys:
+                                    @"BLE設定", @"title",
+                                    [NSArray arrayWithObjects:
+                                            [NSDictionary dictionaryWithObject:@"ペアリング要求" forKey:@"title"],
+                                            [NSDictionary dictionaryWithObject:@"ペアリング解除要求" forKey:@"title"],
+                                            nil], @"children",
+                                    [NSNumber numberWithBool:YES], @"header",
+                                    nil],
+                            [NSDictionary dictionaryWithObjectsAndKeys:
+                                    @"ユーティリティー", @"title",
+                                    [NSArray arrayWithObjects:
+                                            [NSDictionary dictionaryWithObject:@"ツールのバージョンを参照" forKey:@"title"],
+                                            [NSDictionary dictionaryWithObject:@"ツールのログファイルを参照" forKey:@"title"],
+                                            nil], @"children",
+                                    [NSNumber numberWithBool:YES], @"header",
+                                    nil],
+                            nil];
+        [self setSidebarItems:array];
+        [[self sidebar] setFloatsGroupRows:NO];
+        [[self sidebar] expandItem:nil expandChildren:YES];
+
     }
 
     - (BOOL)applicationShouldTerminateAfterLastWindowClosed:(NSApplication *)sender {
@@ -43,6 +70,25 @@
 
     - (IBAction)menuItemVendorDidSelect:(id)sender {
         // TODO: ベンダー向け機能画面を開く
+    }
+
+#pragma mark - Delegate methods for NSOutlineViewDelegate
+
+    - (BOOL)outlineViewItemIsHeader:(id)item {
+        return [[[item representedObject] objectForKey:@"header"] boolValue];
+    }
+
+    - (NSView *) outlineView:(NSOutlineView *)outlineView viewForTableColumn:(NSTableColumn *)tableColumn item:(id)item {
+        bool isHeader = [self outlineViewItemIsHeader:item];
+        return [outlineView makeViewWithIdentifier:(isHeader ? @"HeaderCell" : @"DataCell") owner:self];
+    }
+
+    - (BOOL) outlineView:(NSOutlineView *)outlineView isGroupItem:(id)item {
+        return [self outlineViewItemIsHeader:item];
+    }
+
+    - (BOOL) outlineView:(NSOutlineView *)outlineView shouldSelectItem:(id)item {
+        return ![self outlineViewItemIsHeader:item];
     }
 
 @end
