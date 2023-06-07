2a9cd75c369d74c152603f9d553c59a013734019
Makoto Morita <makmorit@outlook.jp>
2023-06-07 14:52:26 +0900

    機能実装
    ・メニュー項目を表示させる処理の実装
    （データの持ち方は仮の実装です。）

--- DesktopTools/dotNET/DesktopToolApp/DesktopToolApp/SideMenuViewModel.cs
+++ DesktopTools/dotNET/DesktopToolApp/DesktopToolApp/SideMenuViewModel.cs
@@ -1,8 +1,41 @@
-﻿namespace DesktopTool
+﻿using System.Collections.Generic;
+
+namespace DesktopTool
 {
     internal class SideMenuViewModel : ViewModelBase
     {
         public SideMenuViewModel() {
+        }
+
+        public string MenuGroupName {
+            get {
+                // TODO: 仮の実装です
+                return "ツール情報";
+            }
+            set { } 
+        }
+
+        public List<MenuItemTemplate> MenuItems
+        {
+            get {
+                // TODO: 仮の実装です
+                List<MenuItemTemplate> menuItems = new List<MenuItemTemplate>();
+                menuItems.Add(new MenuItemTemplate("ツールのバージョン", "Resources\\information.png"));
+                menuItems.Add(new MenuItemTemplate("ログファイル参照", "Resources\\action_log.png"));
+                return menuItems; 
+            }
+            set { }
+        }
+    }
+
+    internal class MenuItemTemplate
+    {
+        public string ItemName { get; set; }
+        public string Image { get; set; }
+
+        public MenuItemTemplate(string itemName, string image) {
+            ItemName = itemName;
+            Image = image;
         }
     }
 }
