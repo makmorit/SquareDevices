dae5cc310ec2aa60b8c0307c24ee0976c9025ae2
Makoto Morita <makmorit@outlook.jp>
2023-06-07 11:57:44 +0900

    機能実装
    ・サイドメニュー画面を新設
    （中身は仮の実装です。）

---
+++ DesktopTools/dotNET/DesktopToolApp/DesktopToolApp/SideMenuView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace DesktopTool
+{
+    /// <summary>
+    /// SideMenuView.xaml の相互作用ロジック
+    /// </summary>
+    public partial class SideMenuView : UserControl
+    {
+        public SideMenuView()
+        {
+            InitializeComponent();
+        }
+    }
+}
