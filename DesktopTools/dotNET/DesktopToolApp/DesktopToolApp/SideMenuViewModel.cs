487a6deae23346f313a8b702a1da6661a711e37c
Makoto Morita <makmorit@outlook.jp>
2023-06-08 17:01:48 +0900

    機能実装
    ・macOS版ツールと同様のメニュー項目・アイコンを配置

--- DesktopTools/dotNET/DesktopToolApp/DesktopToolApp/SideMenuViewModel.cs
+++ DesktopTools/dotNET/DesktopToolApp/DesktopToolApp/SideMenuViewModel.cs
@@ -4,27 +4,52 @@ namespace DesktopTool
 {
     internal class SideMenuViewModel : ViewModelBase
     {
-        public SideMenuViewModel() {
+        public SideMenuViewModel()
+        {
         }
 
-        public string MenuGroupName {
-            get {
-                // TODO: 仮の実装です
-                return "ツール情報";
-            }
-            set { } 
-        }
-
-        public List<MenuItemTemplate> MenuItems
+        public List<MenuItemGroup> MenuItemGroups
         {
             get {
+                List<MenuItemGroup> menuItemGroups = new List<MenuItemGroup>();
+
                 // TODO: 仮の実装です
-                List<MenuItemTemplate> menuItems = new List<MenuItemTemplate>();
-                menuItems.Add(new MenuItemTemplate("ツールのバージョン", "Resources\\information.png"));
-                menuItems.Add(new MenuItemTemplate("ログファイル参照", "Resources\\action_log.png"));
-                return menuItems; 
+                List<MenuItemTemplate> menuItems1 = new List<MenuItemTemplate>();
+                menuItems1.Add(new MenuItemTemplate("ペアリング実行", "Resources\\connect.png"));
+                menuItems1.Add(new MenuItemTemplate("ペアリング解除要求", "Resources\\disconnect.png"));
+                menuItems1.Add(new MenuItemTemplate("ペアリング情報削除", "Resources\\delete.png"));
+                MenuItemGroup group1 = new MenuItemGroup("BLE設定", menuItems1);
+                menuItemGroups.Add(group1);
+
+                List<MenuItemTemplate> menuItems2 = new List<MenuItemTemplate>();
+                menuItems2.Add(new MenuItemTemplate("ファームウェア更新", "Resources\\update.png"));
+                menuItems2.Add(new MenuItemTemplate("PINGテスト実行", "Resources\\check_box.png"));
+                menuItems2.Add(new MenuItemTemplate("バージョン参照", "Resources\\processor.png"));
+                menuItems2.Add(new MenuItemTemplate("Flash ROM情報参照", "Resources\\statistics.png"));
+                MenuItemGroup group2 = new MenuItemGroup("デバイス保守", menuItems2);
+                menuItemGroups.Add(group2);
+
+                List<MenuItemTemplate> menuItems3 = new List<MenuItemTemplate>();
+                menuItems3.Add(new MenuItemTemplate("ツールのバージョン", "Resources\\information.png"));
+                menuItems3.Add(new MenuItemTemplate("ログファイル参照", "Resources\\action_log.png"));
+                MenuItemGroup group3 = new MenuItemGroup("ツール情報", menuItems3);
+                menuItemGroups.Add(group3);
+
+                return menuItemGroups;
             }
             set { }
+        }
+    }
+
+    internal class MenuItemGroup
+    {
+        public string MenuGroupName { get; set; }
+        public List<MenuItemTemplate> MenuItems { get; set; }
+
+        public MenuItemGroup(string menuGroupName, List<MenuItemTemplate> menuItems)
+        {
+            MenuGroupName = menuGroupName;
+            MenuItems = menuItems;
         }
     }
 
