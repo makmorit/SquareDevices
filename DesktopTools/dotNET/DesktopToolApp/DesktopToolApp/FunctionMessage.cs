777e56bc5c8ecea70abb4b17f5a826e2ab5116fc
Makoto Morita <makmorit@outlook.jp>
2023-06-12 17:05:38 +0900

    機能実装
    ・ツールバージョン参照画面を新設

--- DesktopTools/dotNET/DesktopToolApp/DesktopToolApp/FunctionMessage.cs
+++ DesktopTools/dotNET/DesktopToolApp/DesktopToolApp/FunctionMessage.cs
@@ -5,5 +5,9 @@
         public const string MSG_MENU_ITEM_NAME_TOOL_LOG_FILES = "ログファイル参照";
 
         public const string MSG_FORMAT_ERROR_CANNOT_VIEW_LOG_DIR = "ログファイル格納フォルダーを参照できませんでした。{0}";
+
+        // ツールバージョン参照画面
+        public const string MSG_TOOL_TITLE_FULL = "Square device desktop tool";
+        public const string MSG_VENDOR_TOOL_TITLE_FULL = "Square device vendor tool";
     }
 }
---
+++ DesktopTools/dotNET/DesktopToolApp/DesktopToolApp/ToolVersionInfoViewModel.cs
@@ -0,0 +1,29 @@
+﻿using static DesktopTool.FunctionMessage;
+
+namespace DesktopTool
+{
+    internal class ToolVersionInfoViewModel : ViewModelBase
+    {
+        public string ToolName { 
+            get {
+                if (AppInfoUtil.GetAppBundleNameString().Equals("VendorTool")) {
+                    return MSG_VENDOR_TOOL_TITLE_FULL;
+                } else {
+                    return MSG_TOOL_TITLE_FULL;
+                }
+            } 
+        }
+
+        public string Version { 
+            get {
+                return AppInfoUtil.GetAppVersionString();
+            }
+        }
+
+        public string Copyright { 
+            get { 
+                return AppInfoUtil.GetAppCopyrightString();
+            } 
+        }
+    }
+}
