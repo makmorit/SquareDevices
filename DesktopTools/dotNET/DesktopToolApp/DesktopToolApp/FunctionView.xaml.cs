38bc99abfcdc44cc9b4ba515d472eaf5f923bb66
Makoto Morita <makmorit@outlook.jp>
2023-06-13 15:18:28 +0900

    機能実装
    ・メイン画面右側のビューを管理するクラス（UserControl）の新設

---
+++ DesktopTools/dotNET/DesktopToolApp/DesktopToolApp/FunctionView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace DesktopTool
+{
+    /// <summary>
+    /// FunctionView.xaml の相互作用ロジック
+    /// </summary>
+    public partial class FunctionView : UserControl
+    {
+        public FunctionView()
+        {
+            InitializeComponent();
+        }
+    }
+}
