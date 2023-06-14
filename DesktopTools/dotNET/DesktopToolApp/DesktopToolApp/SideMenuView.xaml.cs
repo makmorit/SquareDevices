90f9f397fed53b77013b166ee95f2b7fbecaa49b
Makoto Morita <makmorit@outlook.jp>
2023-06-14 13:40:34 +0900

    バグ修正
    ・メニュー項目をクリックしても、ビヘイビアが反応しないことがある不具合を解消
    （メニュー項目のクリックイベントが検知されないための不具合。クリックイベントを`SelectedItemChanged`と分離して検知させるよう修正）

--- DesktopTools/dotNET/DesktopToolApp/DesktopToolApp/SideMenuView.xaml.cs
+++ DesktopTools/dotNET/DesktopToolApp/DesktopToolApp/SideMenuView.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace DesktopTool
 {
@@ -7,9 +9,28 @@ namespace DesktopTool
     /// </summary>
     public partial class SideMenuView : UserControl
     {
+        private MenuItemTemplate menuItemTemplate = null!;
+
         public SideMenuView()
         {
             InitializeComponent();
+        }
+
+        private void OnMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            // 業務処理クラスに転送
+            if (menuItemTemplate != null) {
+                FunctionManager.OnMenuItemSelected(menuItemTemplate.ItemName);
+            }
+        }
+
+        private void OnSelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            if (e.NewValue is MenuItemTemplate menuItem) {
+                menuItemTemplate = menuItem;
+            } else {
+                menuItemTemplate = null!;
+            }
         }
     }
 }
