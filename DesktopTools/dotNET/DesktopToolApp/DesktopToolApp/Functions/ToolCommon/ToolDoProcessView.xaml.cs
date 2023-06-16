b95c69196a44b951fa7197891ffe563d54f7d6db
Makoto Morita <makmorit@outlook.jp>
2023-06-16 12:22:38 +0900

    機能実装
    ・処理実行画面（共通画面）の新設

---
+++ DesktopTools/dotNET/DesktopToolApp/DesktopToolApp/Functions/ToolCommon/ToolDoProcessView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace DesktopTool
+{
+    /// <summary>
+    /// ToolDoProcessView.xaml の相互作用ロジック
+    /// </summary>
+    public partial class ToolDoProcessView : UserControl
+    {
+        public ToolDoProcessView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ DesktopTools/dotNET/DesktopToolApp/DesktopToolApp/Functions/ToolCommon/ToolDoProcessViewModel.cs
@@ -0,0 +1,44 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using System.Windows.Input;
+
+namespace DesktopTool
+{
+    internal class ToolDoProcessViewModel : ViewModelBase
+    {
+        // このクラスのインスタンス
+        private static ToolDoProcessViewModel _Instance = new ToolDoProcessViewModel();
+        private readonly RelayCommand _ButtonCloseClickedCommand;
+        private static string _Title = string.Empty;
+
+        public ToolDoProcessViewModel()
+        {
+            _ButtonCloseClickedCommand = new RelayCommand(OnButtonCloseClicked);
+            _Instance = this;
+        }
+
+        public ICommand ButtonCloseClicked
+        {
+            get { return _ButtonCloseClickedCommand; }
+        }
+
+        public static string Title
+        {
+            get { return _Title; }
+            set { _Title = value; }
+        }
+
+        public static ToolDoProcessViewModel Instance
+        {
+            get { return _Instance; }
+        }
+
+        //
+        // 内部処理
+        //
+        private void OnButtonCloseClicked()
+        {
+            // サブ画面を領域から消す
+            FunctionManager.HideFunctionView();
+        }
+    }
+}
