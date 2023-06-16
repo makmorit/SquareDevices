b94a2dd44816d1373a327dc00cf512a0700b44dc
Makoto Morita <makmorit@outlook.jp>
2023-06-16 15:01:45 +0900

    機能実装
    ・テキストボックスに文字を追加し、末尾までスクロールさせるための関数を新設

--- DesktopTools/dotNET/DesktopToolApp/DesktopToolApp/Functions/ToolCommon/ToolDoProcessView.xaml.cs
+++ DesktopTools/dotNET/DesktopToolApp/DesktopToolApp/Functions/ToolCommon/ToolDoProcessView.xaml.cs
@@ -7,9 +7,25 @@ namespace DesktopTool
     /// </summary>
     public partial class ToolDoProcessView : UserControl
     {
+        // このクラスのインスタンス
+        private static ToolDoProcessView _Instance = null!;
+
         public ToolDoProcessView()
         {
             InitializeComponent();
+            _Instance = this;
+        }
+
+        //
+        // 外部公開用
+        //
+        public static void AppendStatusText(string messageText)
+        {
+            // 引数の文字列を、テキストボックス上に表示し改行
+            _Instance.statusText.Text += messageText + "\r\n";
+
+            // テキストボックスの現在位置を末尾に移動
+            _Instance.statusText.ScrollToEnd();
         }
     }
 }
