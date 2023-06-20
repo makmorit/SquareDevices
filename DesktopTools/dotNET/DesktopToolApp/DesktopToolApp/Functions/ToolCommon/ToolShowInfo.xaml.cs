034c1ec609391f2e119d99b7d947d077e7aaf497
Makoto Morita <makmorit@outlook.jp>
2023-06-20 12:13:19 +0900

    機能実装
    ・情報表示画面（共通画面）の新設

---
+++ DesktopTools/dotNET/DesktopToolApp/DesktopToolApp/Functions/ToolCommon/ToolShowInfo.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace DesktopTool.Functions.ToolCommon
+{
+    /// <summary>
+    /// ToolShowInfo.xaml の相互作用ロジック
+    /// </summary>
+    public partial class ToolShowInfo : UserControl
+    {
+        public ToolShowInfo()
+        {
+            InitializeComponent();
+        }
+    }
+}
