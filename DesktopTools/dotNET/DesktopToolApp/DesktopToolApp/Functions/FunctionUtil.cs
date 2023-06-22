02b5dfbb8aee80135b6bce5f016b3fa7e49a7316
Makoto Morita <makmorit@outlook.jp>
2023-06-22 10:29:21 +0900

    リファクタリング
    ・業務処理用のユーティリティークラスを新設

---
+++ DesktopTools/dotNET/DesktopToolApp/DesktopToolApp/Functions/FunctionUtil.cs
@@ -0,0 +1,31 @@
+﻿using AppCommon;
+using System;
+using static DesktopTool.FunctionMessage;
+
+namespace DesktopTool
+{
+    internal class FunctionUtil
+    {
+        //
+        // ログ＋画面項目の両方に、処理開始・終了メッセージを追加出力
+        //
+        public static void ProcessStartLogWithName(string processName, Action<string> AppendStatusText)
+        {
+            string message = string.Format(MSG_FORMAT_START_MESSAGE, processName);
+            AppLogUtil.OutputLogInfo(message);
+            if (AppendStatusText != null) {
+                AppendStatusText(message);
+            }
+        }
+
+        public static void ProcessTerminateLogWithName(string processName, Action<string> AppendStatusText)
+        {
+            string message = string.Format(MSG_FORMAT_END_MESSAGE, processName);
+            AppLogUtil.OutputLogInfo(message);
+            if (AppendStatusText != null) {
+                AppendStatusText(message);
+            }
+        }
+
+    }
+}
