55b88c655e2065d4fd1f4adf932d64a76978adb6
Makoto Morita <makmorit@outlook.jp>
2023-06-26 11:10:06 +0900

    リファクタリング
    ・ネームスペースが間違っているので修正

--- DesktopTools/dotNET/DesktopToolApp/DesktopToolApp/CommonWindow/CommonWindowUtil.cs
+++ DesktopTools/dotNET/DesktopToolApp/DesktopToolApp/CommonWindow/CommonWindowUtil.cs
@@ -3,7 +3,7 @@ using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Interop;
 
-namespace DesktopToolApp.CommonWindow
+namespace DesktopTool.CommonWindow
 {
     internal class CommonWindowUtil
     {
