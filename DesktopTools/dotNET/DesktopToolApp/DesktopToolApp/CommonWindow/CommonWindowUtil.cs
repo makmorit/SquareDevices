bdaa9e6698eeb71f670825cfc5cd009ccc50d6ec
Makoto Morita <makmorit@outlook.jp>
2023-06-27 10:42:34 +0900

    機能実装
    ・入力パスワードチェック用の関数を実装

--- DesktopTools/dotNET/DesktopToolApp/DesktopToolApp/CommonWindow/CommonWindowUtil.cs
+++ DesktopTools/dotNET/DesktopToolApp/DesktopToolApp/CommonWindow/CommonWindowUtil.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Interop;
 
 namespace DesktopTool.CommonWindow
@@ -25,6 +27,30 @@ namespace DesktopTool.CommonWindow
             int style = GetWindowLong(handle, GWL_STYLE);
             style = style & (~WS_SYSMENU);
             SetWindowLong(handle, GWL_STYLE, style);
+        }
+    }
+
+    public class PasswordBoxUtil
+    {
+        public static bool CheckEntrySize(PasswordBox passwordBox, int minSize, int maxSize, string title, string informativeText, Window parentWindow)
+        {
+            int size = passwordBox.Password.Length;
+            if (size < minSize || size > maxSize) {
+                DialogUtil.ShowWarningMessage(parentWindow, title, informativeText);
+                passwordBox.Focus();
+                return false;
+            }
+            return true;
+        }
+
+        public static bool CheckIsNumeric(PasswordBox passwordBox, string title, string informativeText, Window parentWindow)
+        {
+            if (Regex.IsMatch(passwordBox.Password, "^[0-9]*$") == false) {
+                DialogUtil.ShowWarningMessage(parentWindow, title, informativeText);
+                passwordBox.Focus();
+                return false;
+            }
+            return true;
         }
     }
 }
