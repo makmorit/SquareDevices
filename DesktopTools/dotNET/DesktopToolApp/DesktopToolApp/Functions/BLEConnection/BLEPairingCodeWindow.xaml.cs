8958271c9922a44adb0c587f77b03b64585b0847
Makoto Morita <makmorit@outlook.jp>
2023-06-27 09:30:12 +0900

    機能実装
    ・パスコード入力画面を新設

---
+++ DesktopTools/dotNET/DesktopToolApp/DesktopToolApp/Functions/BLEConnection/BLEPairingCodeWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace DesktopTool
+{
+    /// <summary>
+    /// BLEPairingCodeWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class BLEPairingCodeWindow : Window
+    {
+        public BLEPairingCodeWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
