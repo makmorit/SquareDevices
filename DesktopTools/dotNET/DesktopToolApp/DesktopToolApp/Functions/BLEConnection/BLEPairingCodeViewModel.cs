509d4f39b16e21e19673e62d95f46c483774c83f
Makoto Morita <makmorit@outlook.jp>
2023-06-27 09:49:23 +0900

    機能実装
    ・ボタンクリック時の処理を実装

---
+++ DesktopTools/dotNET/DesktopToolApp/DesktopToolApp/Functions/BLEConnection/BLEPairingCodeViewModel.cs
@@ -0,0 +1,40 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using System.Windows.Input;
+
+namespace DesktopTool
+{
+    internal class BLEPairingCodeViewModel : ViewModelBase
+    {
+        private readonly RelayCommand ButtonPairingClickedRelayCommand;
+        private readonly RelayCommand ButtonCloseClickedRelayCommand;
+
+        public BLEPairingCodeViewModel()
+        {
+            ButtonPairingClickedRelayCommand = new RelayCommand(OnButtonPairingClicked);
+            ButtonCloseClickedRelayCommand = new RelayCommand(OnButtonCloseClicked);
+        }
+
+        public ICommand ButtonPairingClicked
+        {
+            get { return ButtonPairingClickedRelayCommand; }
+        }
+
+        public ICommand ButtonCloseClicked
+        {
+            get { return ButtonCloseClickedRelayCommand; }
+        }
+
+        //
+        // 内部処理
+        //
+        private void OnButtonPairingClicked()
+        {
+            BLEPairingCode.OnPairing(this);
+        }
+
+        private void OnButtonCloseClicked()
+        {
+            BLEPairingCode.OnCancel(this);
+        }
+    }
+}
