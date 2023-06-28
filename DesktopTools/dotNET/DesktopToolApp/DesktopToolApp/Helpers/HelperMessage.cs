85eb32ff21eaa70d6715d198f96ada3194918e69
Makoto Morita <makmorit@outlook.jp>
2023-06-28 13:23:24 +0900

    バグ修正
    ・メッセージが改行されないよう修正

--- DesktopTools/dotNET/DesktopToolApp/DesktopToolApp/Helpers/HelperMessage.cs
+++ DesktopTools/dotNET/DesktopToolApp/DesktopToolApp/Helpers/HelperMessage.cs
@@ -6,7 +6,7 @@
         public const string MSG_BLE_PARING_ERR_BT_STATUS_CANNOT_GET = "Bluetooth状態を確認できません。";
         public const string MSG_BLE_PARING_ERR_BT_OFF = "Bluetoothがオフになっています。Bluetoothをオンにしてください。";
         public const string MSG_BLE_PARING_ERR_TIMED_OUT = "ペアリング対象のBLEデバイスが停止している可能性があります。BLEデバイスの電源を入れ、PCのUSBポートから外してください。";
-        public const string MSG_BLE_PARING_ERR_ALREADY_PAIRED = "ペアリング対象のBLEデバイスと既にペアリングされていたようです。\nWindowsのBluetooth環境設定画面から、デバイス「{0}」を削除した後、ペアリングを再実行してください。";
+        public const string MSG_BLE_PARING_ERR_ALREADY_PAIRED = "ペアリング対象のBLEデバイスと既にペアリングされていたようです。WindowsのBluetooth環境設定画面から、デバイス「{0}」を削除した後、ペアリングを再実行してください。";
         public const string MSG_BLE_PARING_ERR_PROCESS = "BLEデバイスとのペアリング時にエラーが発生しました。";
         public const string MSG_BLE_PARING_ERR_UNKNOWN = "BLEデバイスとのペアリング時に不明なエラーが発生しました。";
     }
