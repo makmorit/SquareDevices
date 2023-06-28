c2b358f7733d9a3f7092a68ad3597c00dbf07578
Makoto Morita <makmorit@outlook.jp>
2023-06-28 14:43:50 +0900

    機能修正
    ・ペアリング主処理の制御を変更
    （ユーザーによる中止指示時と、ペアリング前提無し時との扱いを分けるよう修正）

--- DesktopTools/dotNET/DesktopToolApp/DesktopToolApp/Functions/BLEConnection/BLEPairing.cs
+++ DesktopTools/dotNET/DesktopToolApp/DesktopToolApp/Functions/BLEConnection/BLEPairing.cs
@@ -1,4 +1,7 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Security;
+using System.Threading.Tasks;
+using System.Windows;
 using static DesktopTool.FunctionMessage;
 
 namespace DesktopTool
@@ -34,7 +37,55 @@ namespace DesktopTool
 
             // 成功時はログ出力
             LogAndShowInfoMessage(MSG_BLE_PAIRING_SCAN_SUCCESS);
-            ResumeProcess(true);
+
+            // ペアリング処理を実行
+            BLEPairingProcessParam processParam = new BLEPairingProcessParam(parameter.BluetoothAddress);
+            new BLEPairingProcess().DoProcess(processParam, OnRequestPairingCode, OnNotifyCommandTerminate);
+        }
+
+        //
+        // コールバック関数
+        //
+        public void OnRequestPairingCode(BLEPairingProcessParam parameter)
+        {
+            // パスコード入力画面を表示
+            Application.Current.Dispatcher.Invoke(new Action(() => {
+                ShowPairingCodeWindow(parameter);
+            }));
+        }
+
+        private void ShowPairingCodeWindow(BLEPairingProcessParam parameter)
+        {
+            // パスコード入力画面を表示
+            BLEPairingCode code = new BLEPairingCode();
+            if (code.OpenForm()) {
+                parameter.SecurePasscode = code.Passcode;
+                parameter.CancelPairing = false;
+            } else {
+                parameter.SecurePasscode = new SecureString();
+                parameter.CancelPairing = true;
+            }
+
+            // ヘルパークラスに制御を戻す
+            BLEPairingProcess.EnterPairingCode(parameter);
+        }
+
+        public void OnNotifyCommandTerminate(bool success, string errorMessage, BLEPairingProcessParam parameter)
+        {
+            if (parameter.CancelPairing) {
+                // ユーザー中止時に固有のメッセージを出力／表示
+                LogAndShowErrorMessage(errorMessage);
+
+                // 閉じるボタンを使用可能に設定
+                FunctionUtil.EnableButtonClickOnApp(true, ViewModel.EnableButtonClose);
+
+            } else {
+                if (success == false) {
+                    // エラー発生時に固有のメッセージを出力／表示
+                    LogAndShowErrorMessage(errorMessage);
+                }
+                ResumeProcess(success);
+            }
         }
     }
 }
