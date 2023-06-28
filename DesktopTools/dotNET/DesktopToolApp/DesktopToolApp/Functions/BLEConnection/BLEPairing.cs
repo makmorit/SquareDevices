e6a7a7b5078485b48b4b2779a61918d8ef0fd543
Makoto Morita <makmorit@outlook.jp>
2023-06-28 14:37:25 +0900

    リファクタリング
    ・パラメーター`BLEPairingParameter`は不要なので削除

--- DesktopTools/dotNET/DesktopToolApp/DesktopToolApp/Functions/BLEConnection/BLEPairing.cs
+++ DesktopTools/dotNET/DesktopToolApp/DesktopToolApp/Functions/BLEConnection/BLEPairing.cs
@@ -3,23 +3,8 @@ using static DesktopTool.FunctionMessage;
 
 namespace DesktopTool
 {
-    internal class BLEPairingParameter
-    {
-        public ulong BluetoothAddress { get; set; }
-        public string ErrorMessage { get; set; }
-
-        public BLEPairingParameter()
-        {
-            BluetoothAddress = 0;
-            ErrorMessage = string.Empty;
-        }
-    }
-
     internal class BLEPairing : ToolDoProcess
     {
-        // パラメーターを保持
-        private BLEPairingParameter Parameter = null!;
-
         public BLEPairing(string menuItemName) : base(menuItemName) { }
 
         protected override void InvokeProcessOnSubThread()
@@ -33,11 +18,6 @@ namespace DesktopTool
 
         private void OnBLEPeripheralScanned(bool success, string errorMessage, BLEPeripheralScannerParam parameter)
         {
-            // Bluetoothアドレス、エラーメッセージを設定
-            Parameter = new BLEPairingParameter();
-            Parameter.BluetoothAddress = parameter.BluetoothAddress;
-            Parameter.ErrorMessage = errorMessage;
-
             if (success == false) {
                 // 失敗時はログ出力
                 LogAndShowErrorMessage(errorMessage);
