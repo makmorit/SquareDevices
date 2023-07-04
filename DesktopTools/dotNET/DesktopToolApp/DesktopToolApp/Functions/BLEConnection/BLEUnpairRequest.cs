d897c11a05d151cb577530f25b7cb3287725aea6
Makoto Morita <makmorit@outlook.jp>
2023-07-04 15:23:35 +0900

    機能実装
    ・ビュー表示処理を実装
    ・タイトル表示処理を実装

--- DesktopTools/dotNET/DesktopToolApp/DesktopToolApp/Functions/BLEConnection/BLEUnpairRequest.cs
+++ DesktopTools/dotNET/DesktopToolApp/DesktopToolApp/Functions/BLEConnection/BLEUnpairRequest.cs
@@ -16,6 +16,33 @@
 
             // メニュー項目名称を保持
             MenuItemName = menuItemName;
+
+            // 画面表示前の処理を実行
+            Instance.PreProcess();
+        }
+
+        //
+        // コールバック関数
+        //
+        public static void InitFunctionView(BLEUnpairRequestViewModel model)
+        {
+            Instance.InitFunctionViewInner(model);
+        }
+
+        //
+        // 内部処理
+        //
+        private void PreProcess()
+        {
+            // メイン画面右側の領域にビューを表示
+            FunctionView.SetViewContent(new BLEUnpairRequestView());
+            FunctionManager.ShowFunctionView();
+        }
+
+        private void InitFunctionViewInner(BLEUnpairRequestViewModel model)
+        {
+            // メニュー項目名を画面表示
+            model.ShowTitle(MenuItemName);
         }
     }
 }
--- DesktopTools/dotNET/DesktopToolApp/DesktopToolApp/Functions/BLEConnection/BLEUnpairRequestViewModel.cs
+++ DesktopTools/dotNET/DesktopToolApp/DesktopToolApp/Functions/BLEConnection/BLEUnpairRequestViewModel.cs
@@ -2,5 +2,26 @@
 {
     internal class BLEUnpairRequestViewModel : ViewModelBase
     {
+        private string title = null!;
+
+        public BLEUnpairRequestViewModel()
+        {
+            Title = string.Empty;
+            try { BLEUnpairRequest.InitFunctionView(this); } catch { }
+        }
+
+        public string Title
+        {
+            get { return title; }
+            set { title = value; NotifyPropertyChanged(nameof(Title)); }
+        }
+
+        //
+        // 画面操作処理
+        //
+        public void ShowTitle(string text)
+        {
+            Title = text;
+        }
     }
 }
