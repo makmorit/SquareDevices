3770eb6903cadfc9b18559d090635ab80114215c
Makoto Morita <makmorit@outlook.jp>
2023-07-10 14:24:00 +0900

    機能実装
    ・ファームウェア更新進捗画面を新設

---
+++ DesktopTools/dotNET/DesktopToolApp/DesktopToolApp/Functions/DeviceMaintenance/FWUpdateProcessWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace DesktopTool
+{
+    /// <summary>
+    /// FWUpdateProcessWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class FWUpdateProcessWindow : Window
+    {
+        public FWUpdateProcessWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
