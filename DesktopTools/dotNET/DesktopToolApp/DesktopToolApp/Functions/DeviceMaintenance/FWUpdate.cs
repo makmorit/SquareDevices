77bb880e6a946d69c91f3295e9749ab5b98621d0
Makoto Morita <makmorit@outlook.jp>
2023-07-11 11:30:33 +0900

    機能実装
    ・ファームウェア更新処理クラスを新設

--- DesktopTools/dotNET/DesktopToolApp/DesktopToolApp/Functions/DeviceMaintenance/FWUpdate.cs
+++ DesktopTools/dotNET/DesktopToolApp/DesktopToolApp/Functions/DeviceMaintenance/FWUpdate.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows;
 
 namespace DesktopTool
 {
@@ -36,8 +38,33 @@ namespace DesktopTool
                 return;
             }
 
-            // TODO: 仮の実装です。
-            ResumeProcess(success);
+            // ファームウェア更新進捗画面を表示
+            Application.Current.Dispatcher.Invoke(new Action(() => {
+                ShowFWUpdateProcessWindow(sender);
+            }));
+        }
+
+        //
+        // 内部処理
+        //
+        // ファームウェア更新処理クラスの参照を保持
+        FWUpdateProcess UpdateProcess = null!;
+
+        private void ShowFWUpdateProcessWindow(FWUpdateImage sender)
+        {
+            // ファームウェア更新進捗画面を表示
+            UpdateProcess = new FWUpdateProcess(sender.UpdateImageData);
+            if (UpdateProcess.OpenForm() == false) {
+                // TODO: 仮の実装です。
+                CancelProcess();
+
+            } else {
+                // TODO: 仮の実装です。
+                ResumeProcess(true);
+            }
+
+            // ファームウェア更新処理クラスの参照をクリア
+            UpdateProcess = null!;
         }
     }
 }
---
+++ DesktopTools/dotNET/DesktopToolApp/DesktopToolApp/Functions/DeviceMaintenance/FWUpdateProcess.cs
@@ -0,0 +1,52 @@
+﻿using System.Windows;
+
+namespace DesktopTool
+{
+    internal class FWUpdateProcess
+    {
+        // このクラスのインスタンス
+        public static FWUpdateProcess Instance = null!;
+        private FWUpdateProcessWindow Window = null!;
+        private FWUpdateImageData ImageData = null!;
+
+        public FWUpdateProcess(FWUpdateImageData imageData)
+        {
+            Instance = this;
+            Instance.ImageData = imageData;
+        }
+
+        public bool OpenForm()
+        {
+            // ファームウェア更新進捗画面を、ホーム画面の中央にモード付きで表示
+            Window = new FWUpdateProcessWindow();
+            Window.Owner = Application.Current.MainWindow; ;
+            bool? b = Window.ShowDialog();
+            if (b == null) {
+                return false;
+            } else {
+                return (bool)b;
+            }
+        }
+
+        private void NotifyTerminateInner(bool b, string errorMessage)
+        {
+            // ファームウェア更新進捗画面を閉じる
+            Window.DialogResult = b;
+            Window.Close();
+            Window = null!;
+        }
+
+        //
+        // コールバック関数
+        //
+        public static void InitView(FWUpdateProcessViewModel model)
+        {
+        }
+
+        public static void OnCancel(FWUpdateProcessViewModel model)
+        {
+            // TODO: 仮の実装です。
+            Instance.NotifyTerminateInner(false, string.Empty);
+        }
+    }
+}
