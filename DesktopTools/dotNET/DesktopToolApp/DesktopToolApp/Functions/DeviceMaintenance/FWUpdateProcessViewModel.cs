c0939226367edd7bf50d285d6c6f31c38a670044
Makoto Morita <makmorit@outlook.jp>
2023-07-11 11:55:20 +0900

    機能実装
    ・ファームウェア更新進捗画面のプロパティー設定処理を実装

--- DesktopTools/dotNET/DesktopToolApp/DesktopToolApp/Functions/DeviceMaintenance/FWUpdateProcessViewModel.cs
+++ DesktopTools/dotNET/DesktopToolApp/DesktopToolApp/Functions/DeviceMaintenance/FWUpdateProcessViewModel.cs
@@ -6,10 +6,18 @@ namespace DesktopTool
     internal class FWUpdateProcessViewModel : ViewModelBase
     {
         private readonly RelayCommand ButtonCloseClickedRelayCommand;
+        private string title = null!;
+        private string remaining = null!;
+        private int level = 0;
+        private int maxLevel = 0;
 
         public FWUpdateProcessViewModel()
         {
             ButtonCloseClickedRelayCommand = new RelayCommand(OnButtonCloseClicked);
+            Title = string.Empty;
+            Remaining = string.Empty;
+            Level = 0;
+            MaxLevel = 100;
             try { FWUpdateProcess.InitView(this); } catch { }
         }
 
@@ -18,12 +26,59 @@ namespace DesktopTool
             get { return ButtonCloseClickedRelayCommand; }
         }
 
+        public string Title
+        {
+            get { return title; }
+            set { title = value; NotifyPropertyChanged(nameof(Title)); }
+        }
+
+        public string Remaining
+        {
+            get { return remaining; }
+            set { remaining = value; NotifyPropertyChanged(nameof(Remaining)); }
+        }
+
+        public int Level
+        {
+            get { return level; }
+            set { level = value; NotifyPropertyChanged(nameof(Level)); }
+        }
+
+        public int MaxLevel
+        {
+            get { return maxLevel; }
+            set { maxLevel = value; NotifyPropertyChanged(nameof(MaxLevel)); }
+        }
+
         //
         // 内部処理
         //
         private void OnButtonCloseClicked()
         {
             FWUpdateProcess.OnCancel(this);
+        }
+
+        //
+        // 画面操作処理
+        //
+        public void ShowTitle(string text)
+        {
+            Title = text;
+        }
+
+        public void ShowRemaining(string text)
+        {
+            Remaining = text;
+        }
+
+        public void SetLevel(int value)
+        {
+            Level = value;
+        }
+
+        public void SetMaxLevel(int value)
+        {
+            MaxLevel = value;
         }
     }
 }
