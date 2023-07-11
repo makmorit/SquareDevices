300cb0a4dc25923027cc68d55f79390e0202e961
Makoto Morita <makmorit@outlook.jp>
2023-07-11 12:28:58 +0900

    機能実装
    ・ファームウェア更新進捗画面の初期表示処理を追加

--- DesktopTools/dotNET/DesktopToolApp/DesktopToolApp/Functions/DeviceMaintenance/FWUpdateProcess.cs
+++ DesktopTools/dotNET/DesktopToolApp/DesktopToolApp/Functions/DeviceMaintenance/FWUpdateProcess.cs
@@ -1,7 +1,15 @@
 ﻿using System.Windows;
+using static DesktopTool.FunctionMessage;
+using static DesktopTool.FWUpdateProcessConst;
 
 namespace DesktopTool
 {
+    public class FWUpdateProcessConst
+    {
+        // イメージ反映所要時間（秒）
+        public const int DFU_WAITING_SEC_ESTIMATED = 33;
+    }
+
     internal class FWUpdateProcess
     {
         // このクラスのインスタンス
@@ -41,12 +49,30 @@ namespace DesktopTool
         //
         public static void InitView(FWUpdateProcessViewModel model)
         {
+            // タイトルを画面表示
+            model.ShowTitle(MSG_FW_UPDATE_PROCESSING);
+
+            // 最大待機秒数を設定
+            int level = 100 + DFU_WAITING_SEC_ESTIMATED;
+            model.SetMaxLevel(level);
+
+            // メッセージを初期表示
+            NotifyProgress(model, 0, MSG_FW_UPDATE_PRE_PROCESS);
         }
 
         public static void OnCancel(FWUpdateProcessViewModel model)
         {
             // TODO: 仮の実装です。
             Instance.NotifyTerminateInner(false, string.Empty);
+        }
+
+        //
+        // 内部処理
+        //
+        private static void NotifyProgress(FWUpdateProcessViewModel model, int remaining, string caption)
+        {
+            model.SetLevel(remaining);
+            model.ShowRemaining(caption);
         }
     }
 }
