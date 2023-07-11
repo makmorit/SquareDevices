f3347b6e74db0f45a091f4c7cb610f50fb7caf60
Makoto Morita <makmorit@outlook.jp>
2023-07-11 11:14:28 +0900

    機能実装
    ・ファームウェア更新進捗画面のViewModelを新設
    ・中止ボタンクリックイベントを実装

---
+++ DesktopTools/dotNET/DesktopToolApp/DesktopToolApp/Functions/DeviceMaintenance/FWUpdateProcessViewModel.cs
@@ -0,0 +1,29 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using System.Windows.Input;
+
+namespace DesktopTool
+{
+    internal class FWUpdateProcessViewModel : ViewModelBase
+    {
+        private readonly RelayCommand ButtonCloseClickedRelayCommand;
+
+        public FWUpdateProcessViewModel()
+        {
+            ButtonCloseClickedRelayCommand = new RelayCommand(OnButtonCloseClicked);
+            try { FWUpdateProcess.InitView(this); } catch { }
+        }
+
+        public ICommand ButtonCloseClicked
+        {
+            get { return ButtonCloseClickedRelayCommand; }
+        }
+
+        //
+        // 内部処理
+        //
+        private void OnButtonCloseClicked()
+        {
+            FWUpdateProcess.OnCancel(this);
+        }
+    }
+}
