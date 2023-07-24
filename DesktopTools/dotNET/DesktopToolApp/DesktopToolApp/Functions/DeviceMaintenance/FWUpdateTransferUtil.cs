77cdc7d5eebba30e8095a616f66139334bdff291
Makoto Morita <makmorit@outlook.jp>
2023-07-24 18:01:35 +0900

    機能実装
    ・SMPサービス関連のユーティリティークラスを新設

---
+++ DesktopTools/dotNET/DesktopToolApp/DesktopToolApp/Functions/DeviceMaintenance/FWUpdateTransferUtil.cs
@@ -0,0 +1,21 @@
+﻿namespace DesktopTool
+{
+    internal class FWUpdateTransferUtil
+    {
+        //
+        // BLE SMPサービス関連
+        //
+        public static byte[] BuildSMPHeader(byte op, byte flags, ushort len, ushort group, byte seq, byte id_int)
+        {
+            byte[] header = {
+                op,
+                flags,
+                (byte)(len >> 8),   (byte)(len & 0xff),
+                (byte)(group >> 8), (byte)(group & 0xff),
+                seq,
+                id_int
+            };
+            return header;
+        }
+    }
+}
