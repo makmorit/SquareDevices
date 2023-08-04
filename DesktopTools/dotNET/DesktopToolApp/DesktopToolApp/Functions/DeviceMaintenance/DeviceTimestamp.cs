b6e6f794ed179a250fc4158c2c822e36fb554cdf
Makoto Morita <makmorit@outlook.jp>
2023-08-04 17:00:55 +0900

    機能実装
     ・現在時刻参照コマンド実行処理の実装

--- DesktopTools/dotNET/DesktopToolApp/DesktopToolApp/Functions/DeviceMaintenance/DeviceTimestamp.cs
+++ DesktopTools/dotNET/DesktopToolApp/DesktopToolApp/Functions/DeviceMaintenance/DeviceTimestamp.cs
@@ -1,5 +1,7 @@
 ﻿using System.Threading.Tasks;
 using static DesktopTool.BLEDefines;
+using static DesktopTool.FunctionDefines;
+using static DesktopTool.FunctionMessage;
 
 namespace DesktopTool
 {
@@ -26,8 +28,52 @@ namespace DesktopTool
                 return;
             }
 
+            // コールバックを登録
+            sender.RegisterResponseReceivedHandler(ResponseReceivedHandler);
+
+            // 現在時刻参照コマンドを実行
+            PerformInquiryCommand(sender);
+        }
+
+        private void OnResponseInquiryCommand(BLETransport sender, byte[] responseBytes)
+        {
             // TODO: 仮の実装です。
             TerminateCommand(sender, true, string.Empty);
+        }
+
+        //
+        // コールバック関数
+        //
+        private void ResponseReceivedHandler(BLETransport sender, bool success, string errorMessage, byte responseCMD, byte[] responseBytes)
+        {
+            // レスポンス受信失敗時はエラー扱い
+            if (success == false) {
+                TerminateCommand(sender, false, errorMessage);
+                return;
+            }
+
+            // レスポンスステータスをチェック
+            byte status = responseBytes[0];
+            if (status != CTAP1_ERR_SUCCESS) {
+                TerminateCommand(sender, false, string.Format(MSG_FORMAT_OCCUR_UNKNOWN_ERROR_ST, status));
+                return;
+            }
+
+            if (CommandName.Equals(nameof(PerformInquiryCommand))) {
+                OnResponseInquiryCommand(sender, responseBytes);
+            }
+        }
+
+        //
+        // 内部処理
+        //
+        private string CommandName = string.Empty;
+
+        private void PerformInquiryCommand(BLETransport sender)
+        {
+            // バージョン照会コマンド（１回目）を実行
+            sender.SendRequest(U2F_COMMAND_MSG, new byte[] { VENDOR_COMMAND_GET_TIMESTAMP });
+            CommandName = nameof(PerformInquiryCommand);
         }
 
         //
