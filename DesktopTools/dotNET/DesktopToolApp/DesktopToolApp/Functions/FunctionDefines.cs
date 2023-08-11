e327a5f54043decccfd2e3b510a72d2eea8d2ec6
Makoto Morita <makmorit@outlook.jp>
2023-08-11 18:21:44 +0900

    機能実装
    ・PINGテスト処理クラスの実装

--- DesktopTools/dotNET/DesktopToolApp/DesktopToolApp/Functions/FunctionDefines.cs
+++ DesktopTools/dotNET/DesktopToolApp/DesktopToolApp/Functions/FunctionDefines.cs
@@ -6,6 +6,7 @@
         public const byte CTAP1_ERR_SUCCESS = 0x00;
 
         // U2Fコマンド
+        public const byte U2F_COMMAND_PING = 0x01;
         public const byte U2F_COMMAND_KEEPALIVE = 0x02;
         public const byte U2F_COMMAND_MSG = 0x03;
 
