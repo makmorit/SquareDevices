edeb1bc9139d234d82c025da882bbcd8ad5df84b
Makoto Morita <makmorit@outlook.jp>
2023-08-11 17:36:18 +0900

    Update PingTester.cs
     ・PINGテストに使用するランダムデータを生成する処理の実装

--- DesktopTools/dotNET/DesktopToolApp/DesktopToolApp/Functions/DeviceMaintenance/PingTester.cs
+++ DesktopTools/dotNET/DesktopToolApp/DesktopToolApp/Functions/DeviceMaintenance/PingTester.cs
@@ -1,7 +1,29 @@
-﻿namespace DesktopTool
+﻿using AppCommon;
+using System;
+
+namespace DesktopTool
 {
     internal class PingTester : ToolDoProcess
     {
-        public PingTester(string menuItemName) : base(menuItemName) { }
+        // PINGバイトを保持
+        private Random RandomInst { get; set; }
+        private byte[] PingBytes { get; set; }
+
+        public PingTester(string menuItemName) : base(menuItemName) 
+        {
+            RandomInst = new Random();
+            PingBytes = new byte[100];
+        }
+
+        protected override void InvokeProcessOnSubThread()
+        {
+            // 100バイトのランダムデータを生成
+            RandomInst.NextBytes(PingBytes);
+
+            // TODO: 仮の実装です。
+            string dump = AppLogUtil.DumpMessage(PingBytes, PingBytes.Length);
+            AppLogUtil.OutputLogDebug(string.Format("Ping bytes ({0} bytes)\r\n{1}", PingBytes.Length, dump));
+            PauseProcess(true);
+        }
     }
 }
