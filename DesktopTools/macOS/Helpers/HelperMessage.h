df5ef750e70c8e69bfa0150f1770651763038e7f
Makoto Morita <makmorit@outlook.jp>
2023-10-20 16:49:03 +0900

    機能実装
    ・メッセージリソースを追加

---
+++ DesktopTools/macOS/Helpers/HelperMessage.h
@@ -0,0 +1,13 @@
+//
+//  HelperMessage.h
+//  DesktopTool
+//
+//  Created by Makoto Morita on 2023/10/20.
+//
+#ifndef HelperMessage_h
+#define HelperMessage_h
+
+// BLEペアリング
+#define MSG_BLE_PARING_ERR_BT_OFF                   @"Bluetoothがオフになっています。Bluetoothをオンにしてください。"
+
+#endif /* HelperMessage_h */
