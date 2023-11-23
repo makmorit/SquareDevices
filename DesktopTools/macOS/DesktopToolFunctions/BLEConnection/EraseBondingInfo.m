32da72d83df3b65b1607eadd8904f6ed135226e6
Makoto Morita <makmorit@outlook.jp>
2023-11-23 16:52:22 +0900

    機能実装
    ・BLEサービス接続処理を実装

--- DesktopTools/macOS/DesktopToolFunctions/BLEConnection/EraseBondingInfo.m
+++ DesktopTools/macOS/DesktopToolFunctions/BLEConnection/EraseBondingInfo.m
@@ -5,14 +5,16 @@
 //  Created by Makoto Morita on 2023/11/23.
 //
 #import "BLEDefines.h"
+#import "BLEPeripheralRequester.h"
 #import "BLEPeripheralScanner.h"
 #import "EraseBondingInfo.h"
 #import "HelperMessage.h"
 
-@interface EraseBondingInfo () <BLEPeripheralScannerDelegate>
+@interface EraseBondingInfo () <BLEPeripheralScannerDelegate, BLEPeripheralRequesterDelegate>
     // 上位クラスの参照を保持
     @property (nonatomic) id                             delegate;
     @property (nonatomic) BLEPeripheralScanner          *scanner;
+    @property (nonatomic) BLEPeripheralRequester        *requester;
 
 @end
 
@@ -22,6 +24,7 @@
         self = [super initWithDelegate:delegate];
         if (self) {
             [self setScanner:[[BLEPeripheralScanner alloc] initWithDelegate:self]];
+            [self setRequester:[[BLEPeripheralRequester alloc] initWithDelegate:self]];
         }
         return self;
     }
@@ -62,8 +65,23 @@
             [self cancelProcess];
             return;
         }
+        // U2F BLEサービスに接続
+        BLEPeripheralRequesterParam *reqParam = [[BLEPeripheralRequesterParam alloc] initWithConnectedPeripheralRef:[parameter scannedCBPeripheralRef]];
+        [reqParam setServiceUUIDString:U2F_BLE_SERVICE_UUID_STR];
+        [reqParam setCharForSendUUIDString:U2F_CONTROL_POINT_CHAR_UUID_STR];
+        [reqParam setCharForNotifyUUIDString:U2F_STATUS_CHAR_UUID_STR];
+        [[self requester] peripheralWillPrepareWithParam:reqParam];
+    }
+
+    - (void)peripheralDidPrepareWithParam:(BLEPeripheralRequesterParam *)parameter {
         // BLE接続を切断
         [[self scanner] connectedPeripheralWillDisconnect];
+        if ([parameter success] == false) {
+            // 失敗時はログ出力・実行ボタンは再押下可能とする
+            [self LogAndShowErrorMessage:[parameter errorMessage]];
+            [self pauseProcess:false];
+            return;
+        }
         // TODO: 仮の実装です。
         [self resumeProcess:true];
     }
