025149b4eeec0c5731f69c0c94a530db6d99f68c
Makoto Morita <makmorit@outlook.jp>
2023-11-23 16:11:27 +0900

    機能実装
    ・ペアリング情報削除機能クラスの新設

---
+++ DesktopTools/macOS/DesktopToolFunctions/BLEConnection/EraseBondingInfo.m
@@ -0,0 +1,22 @@
+//
+//  EraseBondingInfo.m
+//  DesktopTool
+//
+//  Created by Makoto Morita on 2023/11/23.
+//
+#import "EraseBondingInfo.h"
+
+@interface EraseBondingInfo ()
+    // 上位クラスの参照を保持
+    @property (nonatomic) id                             delegate;
+
+@end
+
+@implementation EraseBondingInfo
+
+    - (instancetype)initWithDelegate:(id)delegate {
+        self = [super initWithDelegate:delegate];
+        return self;
+    }
+
+@end
