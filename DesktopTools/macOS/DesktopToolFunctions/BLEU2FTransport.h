502a1339a26b0ad835cfa3902afa0ac322178079
Makoto Morita <makmorit@outlook.jp>
2023-11-30 17:25:29 +0900

    機能実装
    ・BLE U2Fサービスに特化したクラス（派生クラス）を新設

---
+++ DesktopTools/macOS/DesktopToolFunctions/BLEU2FTransport.h
@@ -0,0 +1,16 @@
+//
+//  BLEU2FTransport.h
+//  DesktopTool
+//
+//  Created by Makoto Morita on 2023/11/30.
+//
+#ifndef BLEU2FTransport_h
+#define BLEU2FTransport_h
+
+#import "BLETransport.h"
+
+@interface BLEU2FTransport : BLETransport
+
+@end
+
+#endif /* BLEU2FTransport_h */
---
+++ DesktopTools/macOS/DesktopToolFunctions/BLEU2FTransport.m
@@ -0,0 +1,15 @@
+//
+//  BLEU2FTransport.m
+//  DesktopTool
+//
+//  Created by Makoto Morita on 2023/11/30.
+//
+#import "BLEU2FTransport.h"
+
+@interface BLEU2FTransport ()
+
+@end
+
+@implementation BLEU2FTransport
+
+@end
