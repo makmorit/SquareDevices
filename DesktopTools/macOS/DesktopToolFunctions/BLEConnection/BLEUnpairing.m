b25acd1f7cdaa01bb2bac473ec38669c0e3151c5
Makoto Morita <makmorit@outlook.jp>
2023-12-05 17:13:30 +0900

    機能実装
    ・ペアリング解除要求コマンド（１回目）実行処理を実装

--- DesktopTools/macOS/DesktopToolFunctions/BLEConnection/BLEUnpairing.m
+++ DesktopTools/macOS/DesktopToolFunctions/BLEConnection/BLEUnpairing.m
@@ -6,11 +6,15 @@
 //
 #import "BLEU2FTransport.h"
 #import "BLEUnpairing.h"
+#import "FunctionDefine.h"
+#import "ToolFunctionMessage.h"
 
 @interface BLEUnpairing () <BLETransportDelegate>
     // 上位クラスの参照を保持
     @property (nonatomic) id                            delegate;
     @property (nonatomic) BLEU2FTransport              *transport;
+    // 実行コマンドを保持
+    @property (nonatomic) NSString                     *commandName;
 
 @end
 
@@ -30,17 +34,52 @@
     }
 
     - (void)transportDidConnect:(bool)success withErrorMessage:(NSString *)errorMessage {
-        // TODO: 仮の実装です。
-        [self disconnectAndResumeProcess:true];
+        if (success == false) {
+            // U2F BLEサービスに接続失敗時はログ出力
+            [self LogAndShowErrorMessage:errorMessage];
+            [self disconnectAndResumeProcess:false];
+            return;
+        }
+        // ペアリング解除要求コマンド（１回目）を実行
+        [self performInquiryCommand];
     }
 
     - (void)transportDidReceiveResponse:(bool)success withErrorMessage:(NSString *)errorMessage withCMD:(uint8_t)responseCMD withData:(NSData *)responseData {
+        if (success == false) {
+            // コマンド受信失敗時はログ出力
+            [self LogAndShowErrorMessage:errorMessage];
+            [self disconnectAndResumeProcess:false];
+            return;
+        }
+        // レスポンスデータを抽出
+        uint8_t *responseBytes = (uint8_t *)[responseData bytes];
+        // ステータスをチェック
+        uint8_t status = responseBytes[0];
+        if (status != CTAP1_ERR_SUCCESS) {
+            NSString *errorMessage = [NSString stringWithFormat:MSG_FORMAT_OCCUR_UNKNOWN_ERROR_ST, status];
+            [self LogAndShowErrorMessage:errorMessage];
+            [self disconnectAndResumeProcess:false];
+            return;
+        }
+        // TODO: 仮の実装です。
+        [self disconnectAndResumeProcess:true];
     }
 
     - (void)disconnectAndResumeProcess:(bool)success {
         // BLE接続を切断し、制御を戻す
         [[self transport] transportWillDisconnect];
         [self resumeProcess:success];
+    }
+
+#pragma mark - Perform command
+
+    - (void)performInquiryCommand {
+        // コマンド名を退避
+        [self setCommandName:NSStringFromSelector(_cmd)];
+        // ペアリング解除要求コマンド（１回目）を実行
+        uint8_t requestBytes[] = {VENDOR_COMMAND_UNPAIRING_REQUEST};
+        NSData *requestData = [[NSData alloc] initWithBytes:requestBytes length:sizeof(requestBytes)];
+        [[self transport] transportWillSendRequest:U2F_COMMAND_MSG withData:requestData];
     }
 
 @end
