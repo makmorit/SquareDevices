360a3d251e7feb1b92330dc0b3f85f8535151e13
Makoto Morita <makmorit@outlook.jp>
2023-12-15 14:39:14 +0900

    機能実装
    ・バージョン照会コマンドのレスポンスから、バージョン情報を抽出する処理を実装

--- DesktopTools/macOS/Functions/DeviceMaintenance/FWVersion.m
+++ DesktopTools/macOS/Functions/DeviceMaintenance/FWVersion.m
@@ -4,10 +4,12 @@
 //
 //  Created by Makoto Morita on 2023/12/15.
 //
+#import "AppUtil.h"
 #import "BLEU2FTransport.h"
 #import "FunctionDefine.h"
 #import "FunctionMessage.h"
 #import "FWVersion.h"
+#import "ToolLogFile.h"
 
 @interface FWVersion () <BLETransportDelegate>
     // 上位クラスの参照を保持
@@ -61,6 +63,8 @@
         }
         // コマンド名により処理分岐
         if ([[self commandName] isEqualToString:@"performInquiryCommand"]) {
+            // バージョン情報をレスポンスから抽出
+            [self extractVersionInquiry:responseData];
             // TODO: 仮の実装です。
             [self disconnectAndTerminateCommand:true withErrorMessage:nil];
         }
@@ -88,6 +92,47 @@
         uint8_t requestBytes[] = {VENDOR_COMMAND_GET_APP_VERSION};
         NSData *requestData = [[NSData alloc] initWithBytes:requestBytes length:sizeof(requestBytes)];
         [[self transport] transportWillSendRequest:U2F_COMMAND_MSG withData:requestData];
+    }
+
+    - (void)extractVersionInquiry:(NSData *)responseData {
+        // 戻りメッセージから、取得情報CSVを抽出
+        NSData *responseBytes = [AppUtil extractCBORBytesFromResponse:responseData];
+        NSString *responseCSV = [[NSString alloc] initWithData:responseBytes encoding:NSASCIIStringEncoding];
+        // 情報取得CSVからバージョン情報を抽出
+        NSArray<NSString *> *array = [self extractValuesFromVersionInfo:responseCSV];
+    }
+
+    - (NSArray<NSString *> *)extractValuesFromVersionInfo:(NSString *)versionInfoCSV {
+        // 情報取得CSVからバージョン情報を抽出
+        NSString *strDeviceName = @"";
+        NSString *strFWRev = @"";
+        NSString *strHWRev = @"";
+        NSString *strFWBuild = @"";
+        for (NSString *element in [versionInfoCSV componentsSeparatedByString:@","]) {
+            NSArray *items = [element componentsSeparatedByString:@"="];
+            NSString *key = [items objectAtIndex:0];
+            NSString *val = [items objectAtIndex:1];
+            if ([key isEqualToString:@"DEVICE_NAME"]) {
+                strDeviceName = [self extractCSVItem:val];
+            } else if ([key isEqualToString:@"FW_REV"]) {
+                strFWRev = [self extractCSVItem:val];
+            } else if ([key isEqualToString:@"HW_REV"]) {
+                strHWRev = [self extractCSVItem:val];
+            } else if ([key isEqualToString:@"FW_BUILD"]) {
+                strFWBuild = [self extractCSVItem:val];
+            }
+        }
+        return @[strDeviceName, strFWRev, strHWRev, strFWBuild];
+    }
+
+    - (NSString *)extractCSVItem:(NSString *)val {
+        // 文字列の前後に２重引用符が含まれていない場合は終了
+        if ([val length] < 2) {
+            return val;
+        }
+        // 取得した項目から、２重引用符を削除
+        NSString *item = [val stringByReplacingOccurrencesOfString:@"\"" withString:@""];
+        return item;
     }
 
 @end
