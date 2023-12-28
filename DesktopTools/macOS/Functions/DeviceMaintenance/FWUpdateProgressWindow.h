d10733ec8ab9f2fd2c5cfaeced0cd39689464c9a
Makoto Morita <makmorit@outlook.jp>
2023-12-28 17:57:50 +0900

    機能実装
    ・ファームウェア更新進捗画面を新設

---
+++ DesktopTools/macOS/Functions/DeviceMaintenance/FWUpdateProgressWindow.h
@@ -0,0 +1,16 @@
+//
+//  FWUpdateProgressWindow.h
+//  MaintenanceTool
+//
+//  Created by Makoto Morita on 2023/12/28.
+//
+#ifndef FWUpdateProgressWindow_h
+#define FWUpdateProgressWindow_h
+
+#import <Cocoa/Cocoa.h>
+
+@interface FWUpdateProgressWindow : NSWindowController
+
+@end
+
+#endif /* FWUpdateProgressWindow_h */
---
+++ DesktopTools/macOS/Functions/DeviceMaintenance/FWUpdateProgressWindow.m
@@ -0,0 +1,45 @@
+//
+//  FWUpdateProgressWindow.m
+//  MaintenanceTool
+//
+//  Created by Makoto Morita on 2023/12/28.
+//
+#import "FWUpdateProgressWindow.h"
+
+@interface FWUpdateProgressWindow ()
+
+    @property (assign) IBOutlet NSTextField         *labelTitle;
+    @property (assign) IBOutlet NSTextField         *labelProgress;
+    @property (assign) IBOutlet NSLevelIndicator    *levelIndicator;
+    @property (assign) IBOutlet NSButton            *buttonCancel;
+
+@end
+
+@implementation FWUpdateProgressWindow
+
+    - (void)windowDidLoad {
+        // 画面項目を初期化
+        [super windowDidLoad];
+        [self initFieldValue];
+    }
+
+    - (void)initFieldValue {
+    }
+
+    - (IBAction)buttonCancelDidPress:(id)sender {
+        // 処理がキャンセルされた場合はCancelを戻す
+        [self terminateWindow:NSModalResponseCancel];
+    }
+
+    - (void)terminateWindow:(NSModalResponse)response {
+        // 親画面の参照を取得
+        NSWindow *mainWindow = [[NSApplication sharedApplication] mainWindow];
+        // モーダル終了を親画面に通知
+        [mainWindow endSheet:[self window] returnCode:response];
+        // 画面を閉じる
+        [self close];
+        // 画面項目を初期化
+        [self initFieldValue];
+    }
+
+@end
