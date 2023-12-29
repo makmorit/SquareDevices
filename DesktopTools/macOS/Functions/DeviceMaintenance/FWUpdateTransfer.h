a4e03f6784708dcc3e04847f59093d802b3f065c
Makoto Morita <makmorit@outlook.jp>
2023-12-29 16:31:57 +0900

    機能実装
    ・ファームウェア更新処理を擬似実装
    ・キャンセル要求に対応できるよう実装

--- DesktopTools/macOS/Functions/DeviceMaintenance/FWUpdateTransfer.h
+++ DesktopTools/macOS/Functions/DeviceMaintenance/FWUpdateTransfer.h
@@ -33,6 +33,7 @@ typedef NS_ENUM(NSInteger, FWUpdateTransferStatusType) {
 
     - (instancetype)initWithDelegate:(id)delegate;
     - (void)start;
+    - (void)cancel;
 
 @end
 
--- DesktopTools/macOS/Functions/DeviceMaintenance/FWUpdateTransfer.m
+++ DesktopTools/macOS/Functions/DeviceMaintenance/FWUpdateTransfer.m
@@ -9,6 +9,11 @@
 @interface FWUpdateTransfer ()
     // 上位クラスの参照を保持
     @property (nonatomic) id                            delegate;
+    // 非同期処理用のキュー（画面用／待機処理用）
+    @property (nonatomic) dispatch_queue_t              mainQueue;
+    @property (nonatomic) dispatch_queue_t              subQueue;
+    // ステータスを保持
+    @property (nonatomic) FWUpdateTransferStatusType    status;
 
 @end
 
@@ -18,12 +23,38 @@
         self = [super init];
         if (self) {
             [self setDelegate:delegate];
+            // メインスレッド／サブスレッドにバインドされるデフォルトキューを取得
+            [self setMainQueue:dispatch_get_main_queue()];
+            [self setSubQueue:dispatch_queue_create("jp.makmorit.tools.desktoptool.fwupdatetransfer", DISPATCH_QUEUE_SERIAL)];
         }
         return self;
     }
 
     - (void)start {
+        // 転送処理の前処理を通知
+        [[self delegate] FWUpdateTransfer:self didNotify:TransferStatusStarting];
+        // 転送処理に移行
+        dispatch_async([self subQueue], ^{
+            [self startUpdateTransfer];
+        });
+    }
+
+    - (void)cancel {
+        // ファームウェア更新イメージ転送処理を中止させる
+        [self setStatus:TransferStatusCanceling];
+    }
+
+    - (void)startUpdateTransfer {
         // TODO: 仮の実装です。
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 5; j++) {
+                if ([self status] == TransferStatusCanceling) {
+                    [[self delegate] FWUpdateTransfer:self didNotify:TransferStatusCanceled];
+                    return;
+                }
+                [NSThread sleepForTimeInterval:0.2];
+            }
+        }
         [[self delegate] FWUpdateTransfer:self didNotify:TransferStatusCompleted];
     }
 
