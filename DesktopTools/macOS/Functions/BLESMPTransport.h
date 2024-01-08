b833b93c909b4a9f78f5f3ad6f15af2fb70a75ad
Makoto Morita <makmorit@outlook.jp>
2024-01-08 14:19:38 +0900

    機能実装
    ・クラス`BLESMPTransport`の新設

---
+++ DesktopTools/macOS/Functions/BLESMPTransport.h
@@ -0,0 +1,16 @@
+//
+//  BLESMPTransport.h
+//  DesktopTool
+//
+//  Created by Makoto Morita on 2024/01/08.
+//
+#ifndef BLESMPTransport_h
+#define BLESMPTransport_h
+
+#import "BLETransport.h"
+
+@interface BLESMPTransport : BLETransport
+
+@end
+
+#endif /* BLESMPTransport_h */
---
+++ DesktopTools/macOS/Functions/BLESMPTransport.m
@@ -0,0 +1,15 @@
+//
+//  BLESMPTransport.m
+//  DesktopTool
+//
+//  Created by Makoto Morita on 2024/01/08.
+//
+#import "BLESMPTransport.h"
+
+@interface BLESMPTransport ()
+
+@end
+
+@implementation BLESMPTransport
+
+@end
