99cbc299ef74e73f2e5c004b9892e50bdecc9e92
Makoto Morita <makmorit@outlook.jp>
2024-01-26 11:28:24 +0900

    機能実装
    ・現在時刻参照の機能クラスを新設

---
+++ DesktopTools/macOS/Functions/DeviceMaintenance/DeviceTimestampShow.h
@@ -0,0 +1,16 @@
+//
+//  DeviceTimestampShow.h
+//  DesktopTool
+//
+//  Created by Makoto Morita on 2024/01/26.
+//
+#ifndef DeviceTimestampShow_h
+#define DeviceTimestampShow_h
+
+#import "ToolDoProcess.h"
+
+@interface DeviceTimestampShow : ToolDoProcess
+
+@end
+
+#endif /* DeviceTimestampShow_h */
---
+++ DesktopTools/macOS/Functions/DeviceMaintenance/DeviceTimestampShow.m
@@ -0,0 +1,15 @@
+//
+//  DeviceTimestampShow.m
+//  DesktopTool
+//
+//  Created by Makoto Morita on 2024/01/26.
+//
+#import "DeviceTimestampShow.h"
+
+@interface DeviceTimestampShow ()
+
+@end
+
+@implementation DeviceTimestampShow
+
+@end
