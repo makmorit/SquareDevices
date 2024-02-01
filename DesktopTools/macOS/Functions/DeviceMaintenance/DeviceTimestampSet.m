112c625c3e949a04e5216fa9e4e7b21a30f6e545
Makoto Morita <makmorit@outlook.jp>
2024-02-01 10:27:24 +0900

    機能実装
    ・`DeviceTimestampDelegate`を実装

--- DesktopTools/macOS/Functions/DeviceMaintenance/DeviceTimestampSet.m
+++ DesktopTools/macOS/Functions/DeviceMaintenance/DeviceTimestampSet.m
@@ -4,12 +4,34 @@
 //
 //  Created by Makoto Morita on 2024/02/01.
 //
+#import "DeviceTimestamp.h"
 #import "DeviceTimestampSet.h"
 
-@interface DeviceTimestampSet ()
+@interface DeviceTimestampSet () <DeviceTimestampDelegate>
+    // 上位クラスの参照を保持
+    @property (nonatomic) id                            delegate;
+    @property (nonatomic) DeviceTimestamp              *deviceTimestamp;
 
 @end
 
 @implementation DeviceTimestampSet
+
+    - (instancetype)initWithDelegate:(id)delegate {
+        self = [super initWithDelegate:delegate];
+        if (self) {
+            [self enableClickButtonDoProcess:false];
+            [self setDeviceTimestamp:[[DeviceTimestamp alloc] initWithDelegate:self]];
+        }
+        return self;
+    }
+
+    - (void)DeviceTimestamp:(DeviceTimestamp *)deviceTimestamp didUpdateState:(bool)available {
+        if (available) {
+            [self enableClickButtonDoProcess:true];
+        }
+    }
+
+    - (void)DeviceTimestamp:(DeviceTimestamp *)deviceTimestamp didNotifyResponseQuery:(bool)success withErrorMessage:(NSString *)errorMessage {
+    }
 
 @end
