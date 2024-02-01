3b49f04b57807cd9f915f9780df1d080a75af246
Makoto Morita <makmorit@outlook.jp>
2024-02-01 10:38:23 +0900

    機能実装
    ・処理前に確認ダイアログを開くよう実装

--- DesktopTools/macOS/Functions/DeviceMaintenance/DeviceTimestampSet.m
+++ DesktopTools/macOS/Functions/DeviceMaintenance/DeviceTimestampSet.m
@@ -6,6 +6,8 @@
 //
 #import "DeviceTimestamp.h"
 #import "DeviceTimestampSet.h"
+#import "FunctionMessage.h"
+#import "PopupWindow.h"
 
 @interface DeviceTimestampSet () <DeviceTimestampDelegate>
     // 上位クラスの参照を保持
@@ -29,6 +31,20 @@
         if (available) {
             [self enableClickButtonDoProcess:true];
         }
+    }
+
+    - (void)showPromptForStartProcess {
+        // 処理続行確認ダイアログを開く
+        [[PopupWindow defaultWindow] promptCritical:MSG_DEVICE_TIMESTAMP_SET_PROMPT withInformative:MSG_DEVICE_TIMESTAMP_SET_COMMENT
+                                          forObject:self forSelector:@selector(timestampSetCommandPromptDone)];
+    }
+
+    - (void)timestampSetCommandPromptDone {
+        // ポップアップでデフォルトのNoボタンがクリックされた場合は、以降の処理を行わない
+        if ([[PopupWindow defaultWindow] isButtonNoClicked]) {
+            return;
+        }
+        [super showPromptForStartProcess];
     }
 
     - (void)DeviceTimestamp:(DeviceTimestamp *)deviceTimestamp didNotifyResponseQuery:(bool)success withErrorMessage:(NSString *)errorMessage {
