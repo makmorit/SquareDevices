f9188d33619d2004f402e47763e6336f95ff3f07
Makoto Morita <makmorit@outlook.jp>
2024-02-01 12:08:37 +0900

    機能実装
    ・PINGテストの機能クラスを新設

---
+++ DesktopTools/macOS/Functions/DeviceMaintenance/PingTester.h
@@ -0,0 +1,16 @@
+//
+//  PingTester.h
+//  DesktopTool
+//
+//  Created by Makoto Morita on 2024/02/01.
+//
+#ifndef PingTester_h
+#define PingTester_h
+
+#import "ToolDoProcess.h"
+
+@interface PingTester : ToolDoProcess
+
+@end
+
+#endif /* PingTester_h */
---
+++ DesktopTools/macOS/Functions/DeviceMaintenance/PingTester.m
@@ -0,0 +1,15 @@
+//
+//  PingTester.m
+//  DesktopTool
+//
+//  Created by Makoto Morita on 2024/02/01.
+//
+#import "PingTester.h"
+
+@interface PingTester ()
+
+@end
+
+@implementation PingTester
+
+@end
