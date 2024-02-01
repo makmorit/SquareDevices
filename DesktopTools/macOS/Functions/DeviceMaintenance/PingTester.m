5d3f3b00cb941e2b8b06981769a65b3080ccc2b2
Makoto Morita <makmorit@outlook.jp>
2024-02-01 13:18:19 +0900

    機能実装
    ・機能クラスに`PingTestQueryDelegate`を実装

--- DesktopTools/macOS/Functions/DeviceMaintenance/PingTester.m
+++ DesktopTools/macOS/Functions/DeviceMaintenance/PingTester.m
@@ -5,11 +5,30 @@
 //  Created by Makoto Morita on 2024/02/01.
 //
 #import "PingTester.h"
+#import "PingTestQuery.h"
 
-@interface PingTester ()
+@interface PingTester () <PingTestQueryDelegate>
+    // 上位クラスの参照を保持
+    @property (nonatomic) id                            delegate;
+    @property (nonatomic) PingTestQuery                *pingTestQuery;
 
 @end
 
 @implementation PingTester
+
+    - (instancetype)initWithDelegate:(id)delegate {
+        self = [super initWithDelegate:delegate];
+        if (self) {
+            [self enableClickButtonDoProcess:false];
+            [self setPingTestQuery:[[PingTestQuery alloc] initWithDelegate:self]];
+        }
+        return self;
+    }
+
+    - (void)PingTestQuery:(PingTestQuery *)pingTestQuery didUpdateState:(bool)available {
+        if (available) {
+            [self enableClickButtonDoProcess:true];
+        }
+    }
 
 @end
