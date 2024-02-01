de0c9376c8b8b61a40f14604ea9ce79193da7c0b
Makoto Morita <makmorit@outlook.jp>
2024-02-01 10:15:59 +0900

    機能実装
    ・現在時刻設定の機能クラスを新設

---
+++ DesktopTools/macOS/Functions/DeviceMaintenance/DeviceTimestampSet.h
@@ -0,0 +1,16 @@
+//
+//  DeviceTimestampSet.h
+//  DesktopTool
+//
+//  Created by Makoto Morita on 2024/02/01.
+//
+#ifndef DeviceTimestampSet_h
+#define DeviceTimestampSet_h
+
+#import "ToolDoProcess.h"
+
+@interface DeviceTimestampSet : ToolDoProcess
+
+@end
+
+#endif /* DeviceTimestampSet_h */
---
+++ DesktopTools/macOS/Functions/DeviceMaintenance/DeviceTimestampSet.m
@@ -0,0 +1,15 @@
+//
+//  DeviceTimestampSet.m
+//  DesktopTool
+//
+//  Created by Makoto Morita on 2024/02/01.
+//
+#import "DeviceTimestampSet.h"
+
+@interface DeviceTimestampSet ()
+
+@end
+
+@implementation DeviceTimestampSet
+
+@end
