74730b4157e820d8ea986066885a9d811864b8d7
Makoto Morita <makmorit@outlook.jp>
2024-02-02 17:01:45 +0900

    機能実装
    ・Flash ROM情報参照の機能クラスを新設

---
+++ DesktopTools/macOS/Functions/DeviceMaintenance/DeviceStorageInfo.m
@@ -0,0 +1,15 @@
+//
+//  DeviceStorageInfo.m
+//  DesktopTool
+//
+//  Created by Makoto Morita on 2024/02/02.
+//
+#import "DeviceStorageInfo.h"
+
+@interface DeviceStorageInfo ()
+
+@end
+
+@implementation DeviceStorageInfo
+
+@end
