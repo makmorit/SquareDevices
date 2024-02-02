17bff6d438acf16dbf7c84401561bed2a3759183
Makoto Morita <makmorit@outlook.jp>
2024-02-02 16:56:54 +0900

    リファクタリング
    ・以前の仮実装を一旦削除

--- DesktopTools/macOS/Functions/DeviceMaintenance/FWVersionInfo.m
+++ DesktopTools/macOS/Functions/DeviceMaintenance/FWVersionInfo.m
@@ -5,31 +5,11 @@
 //  Created by Makoto Morita on 2023/06/05.
 //
 #import "FWVersionInfo.h"
-#import "FunctionMessage.h"
 
 @interface FWVersionInfo ()
 
 @end
 
 @implementation FWVersionInfo
-
-    - (instancetype)initWithDelegate:(id)delegate {
-        self = [super initWithDelegate:delegate];
-        if (self) {
-            [self enableClickButtonDoProcess:true];
-        }
-        return self;
-    }
-
-#pragma mark - Process management
-
-    - (void)invokeProcessOnSubQueue {
-        // TODO: 仮の実装です。
-        for (int i = 0; i < 3; i++) {
-            [NSThread sleepForTimeInterval:1.0];
-        }
-        [self LogAndShowInfoMessage:MSG_DEVICE_FW_VERSION_INFO_SHOWING];
-        [self resumeProcess:true];
-    }
 
 @end
