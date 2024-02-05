d898cdb36e5574c7c9f1107d28617c8cc58772f5
Makoto Morita <makmorit@outlook.jp>
2024-02-05 15:38:49 +0900

    機能実装
    ・機能クラスに`FWVersionDelegate`を実装

--- DesktopTools/macOS/Functions/DeviceMaintenance/FWVersionInfo.m
+++ DesktopTools/macOS/Functions/DeviceMaintenance/FWVersionInfo.m
@@ -4,12 +4,34 @@
 //
 //  Created by Makoto Morita on 2023/06/05.
 //
+#import "FWVersion.h"
 #import "FWVersionInfo.h"
 
-@interface FWVersionInfo ()
+@interface FWVersionInfo () <FWVersionDelegate>
+    // 上位クラスの参照を保持
+    @property (nonatomic) id                            delegate;
+    @property (nonatomic) FWVersion                    *fwVersion;
 
 @end
 
 @implementation FWVersionInfo
+
+    - (instancetype)initWithDelegate:(id)delegate {
+        self = [super initWithDelegate:delegate];
+        if (self) {
+            [self enableClickButtonDoProcess:false];
+            [self setFwVersion:[[FWVersion alloc] initWithDelegate:self]];
+        }
+        return self;
+    }
+
+    - (void)FWVersion:(FWVersion *)fwVersion didUpdateState:(bool)available {
+        if (available) {
+            [self enableClickButtonDoProcess:true];
+        }
+    }
+
+    - (void)FWVersion:(FWVersion *)fwVersion didNotifyResponseQuery:(bool)success withErrorMessage:(NSString *)errorMessage {
+    }
 
 @end
