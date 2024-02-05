72e4a537fc299184f3357653a6c8e555bb49a866
Makoto Morita <makmorit@outlook.jp>
2024-02-05 11:40:34 +0900

    機能実装
    ・機能クラスに`DeviceStorageDelegate`を実装

--- DesktopTools/macOS/Functions/DeviceMaintenance/DeviceStorageInfo.m
+++ DesktopTools/macOS/Functions/DeviceMaintenance/DeviceStorageInfo.m
@@ -4,12 +4,31 @@
 //
 //  Created by Makoto Morita on 2024/02/02.
 //
+#import "DeviceStorage.h"
 #import "DeviceStorageInfo.h"
 
-@interface DeviceStorageInfo ()
+@interface DeviceStorageInfo () <DeviceStorageDelegate>
+    // 上位クラスの参照を保持
+    @property (nonatomic) id                            delegate;
+    @property (nonatomic) DeviceStorage                *deviceStorage;
 
 @end
 
 @implementation DeviceStorageInfo
+
+    - (instancetype)initWithDelegate:(id)delegate {
+        self = [super initWithDelegate:delegate];
+        if (self) {
+            [self enableClickButtonDoProcess:false];
+            [self setDeviceStorage:[[DeviceStorage alloc] initWithDelegate:self]];
+        }
+        return self;
+    }
+
+    - (void)DeviceStorage:(DeviceStorage *)deviceStorage didUpdateState:(bool)available {
+        if (available) {
+            [self enableClickButtonDoProcess:true];
+        }
+    }
 
 @end
