eb48487c6b9e5fe13cecc27fe0e144a6f936529e
Makoto Morita <makmorit@outlook.jp>
2024-04-19 15:34:01 +0900

    機能修正
    ・ホームメニューにおけるメニュー領域を表示／非表示化するための関数を新設

--- DesktopTools/dotNET/DesktopToolApp/DesktopToolApp/MainWindowViewModel.cs
+++ DesktopTools/dotNET/DesktopToolApp/DesktopToolApp/MainWindowViewModel.cs
@@ -4,12 +4,32 @@ namespace DesktopTool
 {
     internal class MainWindowViewModel : ViewModelBase
     {
+        // このクラスのインスタンス
+        private static MainWindowViewModel Instance = null!;
         private GridLength menuWidth = new GridLength(200);
+
+        public MainWindowViewModel()
+        {
+            Instance = this;
+        }
 
         public GridLength MenuWidth
         {
             get { return menuWidth; }
             set { menuWidth = value; NotifyPropertyChanged(nameof(MenuWidth)); }
+        }
+
+        //
+        // 外部公開用
+        //
+        public static void HideMenuItemView()
+        {
+            Instance.MenuWidth = new GridLength(0);
+        }
+
+        public static void ShowMenuItemView()
+        {
+            Instance.MenuWidth = new GridLength(200);
         }
 
         public string TitleString
