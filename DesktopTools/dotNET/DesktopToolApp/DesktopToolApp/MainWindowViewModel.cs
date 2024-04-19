8347e66ac3860502e7eae837a1ded92479d51382
Makoto Morita <makmorit@outlook.jp>
2024-04-19 15:30:45 +0900

    機能修正
    ・メニュー表示幅`MenuWidth`を、ViewModel経由で動的指定できるよう修正

--- DesktopTools/dotNET/DesktopToolApp/DesktopToolApp/MainWindowViewModel.cs
+++ DesktopTools/dotNET/DesktopToolApp/DesktopToolApp/MainWindowViewModel.cs
@@ -1,7 +1,17 @@
-﻿namespace DesktopTool
+﻿using System.Windows;
+
+namespace DesktopTool
 {
     internal class MainWindowViewModel : ViewModelBase
     {
+        private GridLength menuWidth = new GridLength(200);
+
+        public GridLength MenuWidth
+        {
+            get { return menuWidth; }
+            set { menuWidth = value; NotifyPropertyChanged(nameof(MenuWidth)); }
+        }
+
         public string TitleString
         {
             get { return AppInfoUtil.GetAppTitleString(); }
