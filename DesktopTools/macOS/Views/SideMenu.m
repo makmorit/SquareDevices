88dc2c8f5633a95945db2ac1bd2a610c6b067b04
Makoto Morita <makmorit@outlook.jp>
2024-04-29 17:00:56 +0900

    リファクタリング
    ・サイドメニュークラスを新設
    （サイドメニューのView参照をメイン画面クラスから分離させるための措置）

---
+++ DesktopTools/macOS/Views/SideMenu.m
@@ -0,0 +1,47 @@
+//
+//  SideMenu.m
+//  DesktopTool
+//
+//  Created by Makoto Morita on 2024/04/29.
+//
+#import "FunctionManager.h"
+#import "SideMenu.h"
+#import "ToolSideMenuView.h"
+
+@interface SideMenu () <ToolSideMenuViewDelegate>
+    // 上位クラスの参照を保持
+    @property (nonatomic) id                             delegate;
+    // サイドメニュー領域の参照を保持
+    @property (nonatomic) ToolSideMenuView              *toolSideMenuView;
+
+@end
+
+@implementation SideMenu
+
+    - (instancetype)initWithDelegate:(id)delegate {
+        self = [super init];
+        if (self != nil) {
+            // サイドメニュー領域のインスタンスを生成
+            [self setDelegate:delegate];
+            [self setToolSideMenuView:[[ToolSideMenuView alloc] initWithDelegate:self withItemsArray:[FunctionManager createMenuItemsArray]]];
+        }
+        return self;
+    }
+
+    - (void)addSideMenuToStackView:(NSView *)stackView withVisibleRect:(NSRect)visibleRect {
+        // サイドバーを表示
+        [[[self toolSideMenuView] view] setFrame:visibleRect];
+        [[[self toolSideMenuView] view] setWantsLayer:YES];
+        // スタックビューに表示
+        [stackView addSubview:[[self toolSideMenuView] view]];
+    }
+
+    - (bool)menuEnabled {
+        return ![self menuHidden];
+    }
+
+    - (void)ToolSideMenuView:(ToolSideMenuView *)sideMenuView didSelectItemWithTitle:(NSString *)title {
+        [[self delegate] SideMenu:self didSelectItemWithTitle:title];
+    }
+
+@end
